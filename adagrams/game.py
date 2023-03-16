3b122f6ff5db750225e68a07c4bbf8527fdd8c21
Samantha Hurtado <samanthahurtado@Samanthas-MacBook-Pro.local>
2023-03-16 14:39:28 -0700

    new first wave with return

--- adagrams/game.py
+++ adagrams/game.py
@@ -3,6 +3,7 @@ def draw_letters():
     letters = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
     weights_placeholders = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
     random_hand = random.choices(letters, weights = weights_placeholders, k = 10)
+    return random_hand
 
 def uses_available_letters(word, letter_bank):
     pass
