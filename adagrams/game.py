41783ee635175b8e1ca288cabab96120e5d0ed9c
Samantha Hurtado <samanthahurtado@Samanthas-MacBook-Pro.local>
2023-03-16 14:31:15 -0700

    adagrams wave_1 first try

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,8 @@
 def draw_letters():
-    pass
+    from random import choices
+    letters = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+    weights_placeholders = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+    random_hand = random.choices(letters, weights = weights_placeholders, k = 10)
 
 def uses_available_letters(word, letter_bank):
     pass
