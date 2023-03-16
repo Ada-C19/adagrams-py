683fbec28482523d583f1fb15490c480d232aeaa
Carolyn Qi <qi.carolyn@gmail.com>
2023-03-16 15:42:14 -0700

    added wave3

--- adagrams/game.py
+++ adagrams/game.py
@@ -54,7 +54,40 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    letter_score = {
+        'A': 1, 
+        'B': 3, 
+        'C': 3, 
+        'D': 2, 
+        'E': 1, 
+        'F': 4, 
+        'G': 2, 
+        'H': 4, 
+        'I': 1, 
+        'J': 8, 
+        'K': 5, 
+        'L': 1, 
+        'M': 3, 
+        'N': 1, 
+        'O': 1, 
+        'P': 3, 
+        'Q': 10, 
+        'R': 1, 
+        'S': 1, 
+        'T': 1, 
+        'U': 1, 
+        'V': 4, 
+        'W': 4, 
+        'X': 8, 
+        'Y': 4, 
+        'Z': 10
+    }
+    final_score = 0
+    for characters in word:
+        final_score += letter_score[characters.upper()]
+    if len(word) >= 7:
+        final_score += 8
+    return final_score
 
 def get_highest_word_score(word_list):
     pass
