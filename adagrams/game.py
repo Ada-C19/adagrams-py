d9725f6bd0d966fe862bc5ae2cb1b2962eeeff85
Airis Wang <airis.wang2014@fgvmail.br>
2023-03-16 17:55:12 -0400

    Defined draw_letters and passed test_wave_01

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,44 @@
+import random
+
 def draw_letters():
-    pass
+    LETTER_POOL = {
+        "A": 9,
+        "B": 2,
+        "C": 2,
+        "D": 4,
+        "E": 12,
+        "F": 2,
+        "G": 3,
+        "H": 2,
+        "I": 9,
+        "J": 1,
+        "K": 1,
+        "L": 4,
+        "M": 2,
+        "N": 6,
+        "O": 8,
+        "P": 2,
+        "Q": 1,
+        "R": 6,
+        "S": 4,
+        "T": 6,
+        "U": 4,
+        "V": 2,
+        "W": 2,
+        "X": 1,
+        "Y": 2,
+        "Z": 1
+    }
+
+    hand_letters = []
+    letters = list(LETTER_POOL.keys())
+    
+    while len(hand_letters) < 10:
+        letter = random.choice(letters)
+        if LETTER_POOL[letter] > 0:
+            LETTER_POOL[letter] -= 1
+            hand_letters.append(letter)
+    return hand_letters
 
 def uses_available_letters(word, letter_bank):
     pass
