e7b5dc3de7d3d8143ed58497206114de130c4ebc
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-03-16 18:52:55 -0400

    implemented functionality for draw_letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,46 @@
+from random import choice
+
 def draw_letters():
-    pass
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+    MAX_LETTERS = 10
+    LETTER_POOL_LIST = []
+    for letter in LETTER_POOL:
+        temp = [letter] * LETTER_POOL[letter]
+        LETTER_POOL_LIST.extend(temp)
+    
+    letters = []
+    for _ in range(MAX_LETTERS):
+        letters.append(choice(LETTER_POOL_LIST))
+    
+    return letters
+
 
 def uses_available_letters(word, letter_bank):
     pass
@@ -9,3 +50,6 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+test = draw_letters()
+print(test)
