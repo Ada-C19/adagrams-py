13675cf57b241d4923f35501aeb23af5c9598f24
Susi Franco <susi.franco32@gmail.com>
2023-03-16 20:01:47 -0700

    code in draw_letters() to pass draw ten letters test

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,56 @@
-def draw_letters():
-    pass
+
+import random
+
+def draw_letters(): 
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+    available_letters = []
+    letters =  []
+    letter_freq = {}
+
+    for letter, number in LETTER_POOL.items():
+        available_letters.append(letter)
+
+
+    while len(letters) <= 9:
+        random_letter = random.choice(available_letters)
+        print(random_letter)
+        letters.append(random_letter)
+        print(letters)
+
+    return letters
+
+
+
+
+
+
 
 def uses_available_letters(word, letter_bank):
     pass
