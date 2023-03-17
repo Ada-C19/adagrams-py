31dc34b3ba0445ef137ce44b94bf553e9b58117b
Kat <kmunson13@gmail.com>
2023-03-17 14:21:39 -0400

    Changed draw_letters to return list of 10 strings

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,5 @@
 def draw_letters():
-    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    return ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']
 
 def uses_available_letters(word, letter_bank):
     pass
