0fe9d7bc6eb1e9d28828f649a8404ac6002bee02
jasmine <jtmsoto@gmail.com>
2023-03-16 18:15:18 -0700

    Update draw_letters function to account for letter frequency limits

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,31 +1,62 @@
 import random
 
-LETTER_BANK = ("A", "A", "A", "A", "A", "A", "A", "A", "A",
-               "B", "B",
-               "C", "C",
-               "D", "D",
-               "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
-               "F", "F",
-               "G", "G", "G",
-               "H", "H",
-               "I", "I", "I", "I", "I", "I", "I", "I", "I",
-               "J",
-               "K",
-               "L", "L", "L", "L",
-               "M", "M",
-               "N", "N", "N", "N", "N", "N",
-               "O", "O", "O", "O", "O", "O", "O", "O",
-               "P", "P",
-               "Q",
-               "R", "R", "R", "R", "R", "R",
-               "S", "S", "S", "S",
-               "T", "T", "T", "T", "T", "T",
-               "U", "U", "U", "U",
-               "V", "V",
-               "W", "W",
-               "X",
-               "Y", "Y",
-               "Z")
+LETTER_BANK = (
+    "A", "A", "A", "A", "A", "A", "A", "A", "A",
+    "B", "B",
+    "C", "C",
+    "D", "D",
+    "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
+    "F", "F",
+    "G", "G", "G",
+    "H", "H",
+    "I", "I", "I", "I", "I", "I", "I", "I", "I",
+    "J",
+    "K",
+    "L", "L", "L", "L",
+    "M", "M",
+    "N", "N", "N", "N", "N", "N",
+    "O", "O", "O", "O", "O", "O", "O", "O",
+    "P", "P",
+    "Q",
+    "R", "R", "R", "R", "R", "R",
+    "S", "S", "S", "S",
+    "T", "T", "T", "T", "T", "T",
+    "U", "U", "U", "U",
+    "V", "V",
+    "W", "W",
+    "X",
+    "Y", "Y",
+    "Z"
+)
+
+LETTER_FREQUENCY = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 
 LENGTH_OF_HAND = 10
 
@@ -34,7 +65,15 @@ def draw_letters():
 
     while len(hand) < LENGTH_OF_HAND:
         i = random.randint(0, len(LETTER_BANK)-1)
-        hand.append(LETTER_BANK[i])
+        current_letter = LETTER_BANK[i]
+        occurences = 0
+
+        if current_letter in hand:
+            for current_letter in hand:
+                occurences += 1
+            if LETTER_FREQUENCY[current_letter] <= occurences:
+                continue
+        hand.append(current_letter)
 
     return hand
 
