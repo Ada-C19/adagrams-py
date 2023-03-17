d78c0dbd7059f1a2a14b5ab58afee2f699cba723
halimAremu <halimat3000@gmail.com>
2023-03-17 04:40:42 -0500

    Implemented draw_letters function

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,22 @@
+import random
+letter_set1 = ["A","I"]* 9
+letter_set2 = ["B","C","F","H","M","P","V","W","Y"] * 2
+letter_set3 = ["D","L","S","U"] * 4
+letter_set4 = ["E"] * 12
+letter_set5 = ["G"] * 3
+letter_set6 = ["J","K","Q","X","Z"]
+letter_set7 = ["N","R","T"] * 6
+letter_set8 = ["O"]* 8
+letters_list = letter_set1 + letter_set2 + letter_set3 + letter_set4\
+               + letter_set5 + letter_set6 + letter_set7 + letter_set8
 def draw_letters():
-    pass
-
+   
+   letter_hand = random.sample(letters_list, 10)
+   return letter_hand
+   
 def uses_available_letters(word, letter_bank):
-    pass
+   
+   pass
 
 def score_word(word):
     pass
