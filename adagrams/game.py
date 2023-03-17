7546417d4a9ad3d38fd5fe0374c2827d1b90d2b2
Gabby <gabrielleyoung10@gmail.com>
2023-03-16 20:18:36 -0500

    Create function that returns a hand of ten random letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,11 @@
+import random 
+
 def draw_letters():
-    pass
+    letters = ["A"] * 9 + ["B"] * 2 + ["C"] * 2 + ["D"] * 4 + ["E"] * 12 + ["F"] * 2 + ["G"] * 3 + ["H"] * 2 + ["I"] * 9 + ["J"] + ["K"] + ["L"] * 4 + ["M"] * 2 + ["N"] * 6 + ["O"] * 8 + ["P"] * 2 + ["Q"] + ["R"] * 6 + ["S"] * 4 + ["T"] * 6 + ["U"] * 4 + ["V"] * 2 + ["W"] * 2 + ["X"] + ["Y"] * 2 + ["Z"]  
+    
+    hand = random.sample(letters, k=10)
+
+    return hand
 
 def uses_available_letters(word, letter_bank):
     pass
