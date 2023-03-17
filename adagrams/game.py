f7d5d616fcdbb60e2abac9590aa4e13d34b6b508
Angelica <angelica.yee57@gmail.com>
2023-03-17 16:28:01 -0500

    wave 1 is perfect except that the fichas in the hand sometimes exceed the max amount

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,62 @@
+
+import random
+from random import choices
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+
+def build_probabilities_of_ocurrence(dictionary):
+    total_letters = 0
+    probability_dictionary = {}
+    
+    if dictionary == {}:
+        return None
+    else: 
+        for i in dictionary.values():
+            total_letters += i
+    
+        for i,j in dictionary.items():
+            probability_dictionary[i]=j/total_letters
+    
+    return probability_dictionary
+
 def draw_letters():
-    pass
+    probabilities = build_probabilities_of_ocurrence(LETTER_POOL)
+    weights = list(probabilities.values())
+    population = list(probabilities.keys())
+    
+    hand = choices(population,weights,k = 10)
+    print(hand)
+
+    return hand
+    
 
 def uses_available_letters(word, letter_bank):
     pass
@@ -9,3 +66,9 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+
+    
+#build_probabilities_of_ocurrence(LETTER_POOL)
+draw_letters()
+#pruebachoices()
