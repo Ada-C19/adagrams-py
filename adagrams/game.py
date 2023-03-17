09567bb73ff3bde838ddcaac42430d99dd4a501e
Anna Lima <annavanwesep@gmail.com>
2023-03-17 12:11:53 -0700

    changed one_list to a local variable

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,9 +1,9 @@
 import random
 letter_pool_list = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A','B','B', 'C', 'C', 'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F', 'G', 'G', 'G', 'H', 'H', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'J', 'K', 'L', 'L', 'L', 'L', 'M', 'M', 'N', 'N', 'N', 'N', 'N', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'P', 'P', 'Q', 'R', 'R', 'R', 'R', 'R', 'R', 'S', 'S', 'S', 'S', 'T', 'T', 'T', 'T', 'T', 'T', 'U', 'U', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'Y', 'Y', 'Z']
 
-one_list = []
 
 def draw_letters():
+    one_list = []
     for i in range(10):
         random_letter = random.choice(letter_pool_list)
         one_list.append(random_letter)
