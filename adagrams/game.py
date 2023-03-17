8e25005959a9276250bdf19791d6473c49414db1
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-03-17 14:36:51 -0700

    add game.py

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,17 @@
 def draw_letters():
     pass
+# Your first task is to build a hand of 10 letters for the user. To do so, implement the function draw_letters in game.py. This method should have the following properties:
+
+# No parameters
+# Returns an array of ten strings
+# Each string should contain exactly one letter
+# These represent the hand of letters that the player has drawn
+# The letters should be randomly drawn from a pool of letters
+# This letter pool should reflect the distribution of letters as described in the table below
+# There are only 2 available C letters, so draw_letters cannot ever return more than 2 Cs
+# Since there are 12 Es but only 1 Z, it should be 12 times as likely for the user to draw an E as a Z
+# Invoking this function should not change the pool of letters
+# Imagine that the user returns their hand to the pool before drawing new letters
 
 def uses_available_letters(word, letter_bank):
     pass
