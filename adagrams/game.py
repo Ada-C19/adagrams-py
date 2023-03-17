c71248398bdc0597c679e2d366ebd1348f55dcec
Limary Gonzalez <limaryg11@gmail.com>
2023-03-17 14:50:58 -0700

    Updated uses_available_letters function

--- adagrams/game.py
+++ adagrams/game.py
@@ -31,32 +31,38 @@ LETTER_POOL = {
 
 def draw_letters():
     ada_gram = LETTER_POOL
-    letter_pool = []
+    potato = []
     hand_as_list = []
-
+    
     for k,v in ada_gram.items():
         for i in range(v):
-            letter_pool.append(k)
-    
+            potato.append(k)
+
     while len(hand_as_list) < 10:
-        hand_as_list.append(random.choice(letter_pool))
-    # print(hand_as_list)
+        random_letter = random.choice(potato)
+        hand_as_list.append(random_letter)
+        potato.remove(random_letter)
+
     return hand_as_list
 
-# draw_letters()
+
 
 def uses_available_letters(word, letter_bank):
-    # word is input string 
-    # letter bank is list of words
-    #letter_bank can have multiple of one letter, 
-    # must remove letter at a time to account for multiple of same letter in bank
+    word_list = []
     for letter in word:
-        if letter in letter_bank:
-            letter_bank.remove(letter)
-            return True
+        word_list.append(letter.upper())
+
+    letter_list = []
+    for letters in letter_bank:
+        letter_list.append(letters)
+
+
+    for letter in word_list:
+        if letter in letter_list:
+            letter_list.remove(letter)
         else:
             return False
-    
+    return True
 
 def score_word(word):
     # word is a string of characters
@@ -64,6 +70,33 @@ def score_word(word):
     # each letter within word has a point value. The number of points 
     # is summed up to represent the total score of word
     # ***if len of word is 7, 8, 9, or 10 then word gets additional 8 points
+    score_chart = {
+        1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+        2: ["D", "G"],
+        3: ["B", "C", "M", "P"],
+        4: ["F", "H", "V", "W", "Y"],
+        5: ["K"],
+        8: ["J", "X"],
+        10: ["Q", "Z"]
+    }
 
-def get_highest_word_score(word_list):
-    pass
+    score =  0
+
+    for k,v in score_chart.items():
+        for letter in word:
+            if letter.upper() in v:
+                score += k
+    
+    if len(word) in range(7,11):
+        score += 8
+    # print(score)
+    return score
+
+# def get_highest_word_score(word_list):
+#     # word list is a list of strings
+#     # must return a tuple that represents the data of winning word
+#     # and its score: tuple should = ("word", score)
+#     # if there is a tie, prefer word with fewest letters 
+#     # unless one word has 10 letters.
+#     # if there are multiple words that are same score
+#     # and same length, pick the first one in supplied list
