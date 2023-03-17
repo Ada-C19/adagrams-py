cb9fb79d98e6345ad96baf5239b768fd3b06d596
Trang Tran <trantrang181197@gmail.com>
2023-03-16 23:15:31 -0700

    write function score_word() and pass test wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -68,7 +68,53 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    LETTER_POINT = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+    }
+    word = word.upper()
+
+    # Calculate total points
+    if word:
+        n = len(word)
+        
+        # Additional 8 points if word has 7 or more letters 
+        if n > 6:
+            points = 8
+        else:
+            points = 0
+
+        # Loop through each letter and calculate total points
+        for i in range(n):
+            points += LETTER_POINT[word[i]]
+    else:
+        points = 0
+    
+    return points
 
 def get_highest_word_score(word_list):
-    pass
+    
