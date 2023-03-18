7d2390efc8bf980db8feb5c770a3fe7cae8d401d
Agnes Kim <agneskim4@gmail.com>
2023-03-17 21:40:07 -0400

    Passing all tests, tuple in wave 4

--- adagrams/game.py
+++ adagrams/game.py
@@ -100,24 +100,30 @@ def score_word(word):
     point_system ={
     "A": 1, 
     "E": 1,
-    "I" : 1, 
+    "I": 1, 
     "O": 1, 
-    "U" :1,
-    "L" : 1,
+    "U": 1,
+    "L": 1,
     "N": 1,
-    "R" : 1,
-    "S" : 1,
-    "T" : 1,
+    "R": 1,
+    "S": 1,
+    "T": 1,
     "D": 2, 
-    "G" : 2,
+    "G": 2,
     "B": 3, 
     "C": 3, 
     "M": 3, 
-    "P":3,
-    "F": 4, "H": 4, "V": 4, "W":4, "Y": 4,
+    "P": 3,
+    "F": 4, 
+    "H": 4, 
+    "V": 4, 
+    "W": 4, 
+    "Y": 4,
     "K": 5,
-    "J": 8, "X":8 ,
-    "Q": 10,"Z": 10
+    "J": 8, 
+    "X": 8,
+    "Q": 10,
+    "Z": 10
     }
 # FUNCTION BODY
     word_capital = word.upper()
@@ -140,7 +146,6 @@ def get_highest_word_score(word_list):
 #     - If the there are multiple words that are the same score and the same length, pick the first one in the supplied list
 
 # FUNCTION BODY
-    best_word = []
     all_scores = {}
     top_word = ""
     top_score = 0
@@ -159,6 +164,5 @@ def get_highest_word_score(word_list):
             elif len(word) < len(top_word) and len(top_word) != 10:
                 top_word = word
                 
-    best_word.append(top_word)
-    best_word.append(top_score)
+    best_word = (top_word, top_score)
     return best_word
