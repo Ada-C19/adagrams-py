1979108e5e61b8b129dc4cf0ee0cf769f7346bfc
Jessie <wajliu3@gmail.com>
2023-03-17 17:18:20 -0700

    complete wave_1

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,52 @@
+import random
+import string
+
+
 def draw_letters():
-    pass
+    LETTER_POOL = ETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+    hand = []
+    count_frequency = {}
+    for letter in LETTER_POOL:
+        count_frequency[letter] = 0
+        
+    i = 0
+    while i < 10:
+        letter = random.choice(string.ascii_letters).capitalize()
+        if count_frequency[letter] < LETTER_POOL[letter]:
+            hand.append(letter)
+            count_frequency[letter] += 1
+            i += 1
+        else:
+            continue
+    return hand 
+    
 
 def uses_available_letters(word, letter_bank):
     pass
