f442e1725e8096b3c3536068041520e85254d46a
Angie <caritocon@gmail.com>
2023-03-18 00:24:37 -0700

    Third function passed

--- adagrams/game.py
+++ adagrams/game.py
@@ -40,9 +40,23 @@ def uses_available_letters(word, letter_bank):
         else:
             return False
     return True
-print(uses_available_letters("DOG", ["D", "O", "X", "X", "X", "X", "X", "X", "X", "X"]))
-# def score_word(word):
-#     pass
+
+def score_word(word):
+    score_chart = {
+                    'A': 1, 'E': 1, 'I': 1, 'O': 1, 'L': 1, 'N': 1, 
+                    'R': 1, 'S': 1, 'T': 1, 'D': 2, 'G': 2,'B': 3, 
+                    'C': 3, 'M': 3, 'P': 3, 'F': 4, 'H': 4, 'V': 4, 
+                    'W': 4, 'Y': 4,'K': 5, 'J': 8, 'X': 8,'Q': 10, 
+                    'Z': 10
+                }
+    list_from_word = list(word.upper())
+    total_points = 0
+    for letter in list_from_word:
+        if letter in score_chart:
+            total_points += score_chart[letter]
+    if len(word) == 7 or len(word) == 8 or len(word) == 9 or len(word) == 10:
+        total_points += 8
+    return total_points
 
 # def get_highest_word_score(word_list):
 #     pass
