4fa87480612d640b0ff828f576d5600257f21f10
Angie <caritocon@gmail.com>
2023-03-17 23:30:39 -0700

    Second Function passed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,11 +1,9 @@
 import random
-# Hacer una funcion que me cree el dict con las 10 letras random
-# Dentro de la 1ra f(x) codigo que compara contra el diccionario ppal y evalua que no se pase del numero max de letras disponibles
+LETTER_POOL = {'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 
+            'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1,'R': 6, 'S': 4, 
+            'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1}
 def create_dict_of_10_letters():
-    alphabet_dict = {'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 
-    'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 
-    'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 
-    'Z': 1}
+    alphabet_dict = LETTER_POOL.copy()
 
     dict_of_10_letters = {}
     total_letters_dict = 0
@@ -22,8 +20,6 @@ def create_dict_of_10_letters():
             alphabet_dict[letter] -= 1
             total_letters_dict += 1
     return dict_of_10_letters
-        
-print(create_dict_of_10_letters())
 
 def draw_letters():
     selected_dict = create_dict_of_10_letters()
@@ -32,13 +28,19 @@ def draw_letters():
         for i in range(num):
             list_of_letters.append(letter)
     return list_of_letters
-print(draw_letters())
 
 def uses_available_letters(word, letter_bank):
-    letters_from_word_list = list(word)
-    print(letters_from_word_list)
-
-print(uses_available_letters("dog", draw_letters()))
+    copy_letter_bank = letter_bank.copy()
+    letters_from_word_list = list(word.upper())
+    while len(letters_from_word_list) > 0:
+        letter = letters_from_word_list[0]
+        if letter in copy_letter_bank:
+            copy_letter_bank.remove(letter)
+            letters_from_word_list.remove(letter)
+        else:
+            return False
+    return True
+print(uses_available_letters("DOG", ["D", "O", "X", "X", "X", "X", "X", "X", "X", "X"]))
 # def score_word(word):
 #     pass
 
