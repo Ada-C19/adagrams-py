69338080b4f0257e6361ab6482c52d7bfcedc760
Whitney <whitneyshake@gmail.com>
2023-03-17 22:19:42 -0700

    Corrected incorrect varible name in Wave 01, 4/4 tests passed in Wave 01

--- adagrams/game.py
+++ adagrams/game.py
@@ -30,31 +30,21 @@ LETTER_POOL = {
 }
 
 player_one = []
+pool_conversion = list(LETTER_POOL)
 
 def draw_letters():
 
-    x = list(LETTER_POOL)
+    re_ordered_list = random.sample(pool_conversion, len(pool_conversion))
+    game_letters = re_ordered_list[:10]
+    return game_letters
 
-    re_ordered_list = random.sample(x, len(x))
-    this_fool = re_ordered_list[:10]
-    return this_fool
-    # while len(player_one) < 10:
-    #     letter = random.choice(list(LETTER_POOL))
-    #     if letter in player_one and player_one.count(letter) >= LETTER_POOL.get(letter):
-    #         continue
-    #     else:
-    #         player_one.append(letter)
-    # return player_one
-
-
-#     return game_letters
-#     pass
-
+# ----------------------------------------------------------------------------
+# letter_bank = draw_letters()
 # def uses_available_letters(word, letter_bank):
-#     pass
+
 
 # def score_word(word):
-#     pass
+    # pass
 
 # def get_highest_word_score(word_list):
-#     pass
+    # pass
