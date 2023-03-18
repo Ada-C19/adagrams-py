dd5365d4f43cf058d72827331b93829f40e61196
Tigist Woldearegai <tigistlina@gmail.com>
2023-03-18 14:24:23 -0500

    Passes wave 1 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,15 +1,22 @@
 import random
 import string
-POOL_OF_LETTERS = {
+import copy
+
+
+def draw_letters():
+    
+    POOL_OF_LETTERS = {
+    
     
     "A" : 9, "N" : 6, "B" : 2, "O" : 8, "C" : 2, "P" : 2, "D" : 4, "Q" : 1, 
     "E" : 12,"R" : 6, "F" : 2, "S" : 4, "G" : 3, "T" : 6, "H" : 2, "U" : 4,
     "I" : 9, "V" : 2, "J" : 1, "W" : 2, "K" : 1, "X" : 1, "L" : 4, "Y" : 2, 
     "M" : 2, "Z" : 1 
     
-}
-
-def draw_letters():
+    }
+    
+    
+    
     picked_letters = []
     random_letter = ""
     
