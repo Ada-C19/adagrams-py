7633115d9fa03f9f52da23bdf63aa7e2d64618b8
Sage Choi <coolcuds@gmail.com>
2023-03-18 11:54:18 -0700

    Added comments and functionality to score_word() to return score of input word

--- adagrams/game.py
+++ adagrams/game.py
@@ -65,7 +65,50 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    # Dictionary of letters and points
+    letter_scores = {
+        'A': 1,
+        'E': 1,
+        'I': 1,
+        'O': 1,
+        'U': 1,
+        'L': 1,
+        'N': 1,
+        'R': 1,
+        'S': 1,
+        'T': 1,
+        'D': 2,
+        'G': 2,
+        'B': 3,
+        'C': 3,
+        'M': 3,
+        'P': 3,
+        'F': 4,
+        'H': 4,
+        'V': 4,
+        'W': 4,
+        'Y': 4,
+        'K': 5,
+        'J': 8,
+        'X': 8,
+        'Q': 10,
+        'Z': 10
+    }
+
+    word_score = 0
+    word = word.upper() # Convert word to uppercase letters
+
+    # Check if the word length rewards an extra 8 points
+    if len(word) in (7, 8, 9, 10):
+        word_score += 8
+
+    # Add value of each letter in word to word_score total
+    for letter in word:
+        if letter in letter_scores:
+            word_score += letter_scores[letter]
+    
+    return word_score
+
 
 def get_highest_word_score(word_list):
     pass
