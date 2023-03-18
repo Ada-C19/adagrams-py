77edd872d523e1d852a3cb8d4debaf84a56c7fc0
chickenandegg <tei.villanueva@gmail.com>
2023-03-17 17:54:05 -0700

    pass test 1 test_draw_letters_draws_ten

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,55 @@
+import random
+
+
 def draw_letters():
-    pass
+    
+    letters = {
+        'A': 9,
+        'B': 2,
+        'C': 2,
+        'D': 4,
+        'E': 12,
+        'F': 2,
+        'G': 3,
+        'H': 2,
+        'I': 9,
+        'J': 1,
+        'K': 1,
+        'L': 4,
+        'M': 2,
+        'N': 6,
+        'O': 8,
+        'P': 2,
+        'Q': 1,
+        'R': 6,
+        'S': 4,
+        'T': 6,
+        'U': 4,
+        'V': 2,
+        'W': 2,
+        'X': 1,
+        'Y': 2,
+        'Z': 1
+    }
+# this variable holds all the letters to draw
+    letter_box = []
+
+ # loop through the dict
+    for letter, number in letters.items():
+        #loop and check if the letter actually matches the frequency being added 
+        for i in range(number):
+            letter_box.append(letter)
+
+        #this holds the 10 letters -- 
+    letter_list = []
+
+## looping through letters using random.choice and appends to the letter_list
+    for letters in range(10):
+        letter = random.choice(letter_box)
+        letter_list.append(letter)
+    # remove will prevent from creating duplicates to be added
+        letter_box.remove(letter)
+    return letter_list
 
 def uses_available_letters(word, letter_bank):
     pass
