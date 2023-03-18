43921edf1d09be000a02cf0b5f82562064e3b0ea
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2023-03-18 15:42:07 -0400

    modify draw_letters function and add while loop to add 10 random letters into a list where letters not exceed the maximum allowed, all test_wave_01 passed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,14 +1,25 @@
 import string
 import random
-
+LETTERS_ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                       'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+LETTERS_MAX_ALLOWED = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1]
 def draw_letters():
     hand = []
     random_letter = ""
-    for i in range(10):
-        random_letter = random.choice(string.ascii_letters).upper()
-        hand.append(random_letter)
+    letters_alphabet = LETTERS_ALPHABET
+    cant_letter = 0
+        
+    while len(hand) < 10:
+        random_letter = random.choice(list(letters_alphabet))
+        for i in range(len(letters_alphabet)):
+            if letters_alphabet[i] == random_letter and cant_letter < LETTERS_MAX_ALLOWED[i]:
+                hand.append(random_letter)
+                cant_letter += 1
+
     return hand
 
+    for i in range(10):
+        random_letter = random_letter()
 def uses_available_letters(word, letter_bank):
     pass
 
