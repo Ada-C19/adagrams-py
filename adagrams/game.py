c9f3cb83f9c0c43abc2878fda39f905f42fe945f
Aisha Mohamed <amdahir1@gmail.com>
2023-03-18 16:36:41 -0500

    3 tests left

--- adagrams/game.py
+++ adagrams/game.py
@@ -29,9 +29,9 @@ def uses_available_letters(word, letter_bank):
     letter_bank_ = []
 
     for i in word:
-        word_.append(i)
+        word_.append(i.casefold())
     for i in letter_bank:
-        letter_bank_.append(i)
+        letter_bank_.append(i.casefold())
 
     counter = 0
     for i in word_:
@@ -48,7 +48,127 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    score = 0
+    # score_1 = ['A', 'E', 'I', 'O', 'U', 'L' ,'N','R','S','T']
+    # score_2 = ['D', 'G']
+    # score_3 = ['B', 'C', 'M', 'P']
+    # score_4 = ['F', 'H', 'V', 'W','Y']
+    # score_5 = ['K']
+    # score_8 = ['J','X']
+    # score_10 = ['Q','Z']
+    score_1 = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'L', 'l','N', 'n', 'R', 'r', 'S','s', 'T','t']
+    score_2 = ['D', 'd', 'G', 'g']
+    score_3 = ['B', 'b', 'C', 'c', 'M', 'm','P', 'p']
+    score_4 = ['F', 'f', 'H', 'h', 'V', 'v', 'W', 'w','Y', 'y']
+    score_5 = ['K', 'k']
+    score_8 = ['J','j','X', 'x']
+    score_10 = ['Q', 'q','Z', 'z']
+
+    word_ =  []
+
+    for i in word:
+        word_.append(i.casefold())
+
+    counter = 0
+    for i in word_: 
+        # if i == 'A' or 'E' or 'I' or 'O' or 'U' or 'L' or 'N' or 'R' or 'S' or 'T':
+        if i in score_1:
+            score += 1
+            # counter += 1
+        # elif i == 'D' or 'G': 
+        elif i in score_2:
+            score += 2
+            # counter += 1
+        # elif i == 'B' or 'C' or 'M' or 'P':
+        elif i in score_3:
+            score += 3  
+            # counter += 1
+        # elif i == 'F' or 'H' or 'V' or 'W' or 'Y':
+        elif i in score_4:
+            score += 4
+            # counter += 1
+        # elif i == 'K':
+        elif i in score_5:
+            score += 5
+            # counter += 1
+        # elif i == 'J' or 'X':
+        elif i in score_8:
+            score += 8
+            # counter += 1
+        # elif i == 'Q' or 'Z':
+        elif i in score_10:
+            score += 10
+            # counter += 1
+    
+    if len(word_) <= 10 and len(word_) >= 7:
+        score += 8
+    
+
+    # if len(word_) == 7 or 8 or 9 or 10: # still dont understand why this doesnt work, come back
+    #     score += 8
+    # else: 
+    #     score += 0
+
+    # if counter == 7 or 8 or 9 or 10:
+    #         score += 8
+    # else:
+    #         score += 0
+
+    # counter = 0
+    # while counter < 1:
+    #     if len(word) == 7 or 8 or 9 or 10:
+    #         score += 8
+    #     counter +=1
+
+    return score
+        
 
 def get_highest_word_score(word_list):
-    pass
+    tuple_list = []
+    for i in word_list:
+        tuple_list.append((i, score_word(i)))
+
+    # for i in tuple_list:
+
+    best_word_list = sorted(tuple_list, key=lambda t: t[1], reverse = True)
+    
+    tie_breaker_list = []
+
+    for i in best_word_list:
+        if i[1] == best_word_list[0][1]:
+            tie_breaker_list.append(i)
+
+    tie_breaker_list_sorted = sorted(tie_breaker_list, key=lambda x: len(x[0]))
+
+    for i in tie_breaker_list_sorted:
+        if len(tie_breaker_list_sorted[0][0]) == len(tie_breaker_list_sorted[1][0]):
+            if tie_breaker_list_sorted[0][0] == word_list[0]:
+                best_word = tie_breaker_list_sorted[0]
+            else:
+                best_word = tie_breaker_list_sorted[1]
+
+                
+        elif len(i[0]) == 10:
+            best_word = i
+        else:
+            best_word = tie_breaker_list_sorted[0]
+
+    return best_word
+    
+
+    # if best_word_list[0] > best_word_list[1]:
+    #     best_word = best_word_list[0]
+    # elif best_word_list[0] == best_word_list[1] and best_word_list[1] > best_word_list[2]:
+    #     if len(best_word_list[0][0]) > len(best_word_list[1][0]):
+    #         best_word = best_word_list[1]
+    #     else:
+    #         best_word = best_word_list[0]
+    # elif best_word_list[0] == best_word_list[1] == best_word_list[2] and best_word_list[2] > best_word_list[3]:
+    #     if len(best_word_list[0][0]) > len(best_word_list[1][0]):
+    #         best_word = best_word_list[1]
+    #     elif len(best_word_list[2][0]) > len(best_word_list[1][0]
+    # elif best_word_list[0] == best_word_list[1] == best_word_list[2] == best_word_list[3] and best_word_list[3] > best_word_list[4]:
+
+
+
+    return best_word
