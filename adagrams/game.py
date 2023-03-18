136234c3198266583c9b9317bfc5cfc37ca02aa4
Sel <selenetenorio27@hotmail.com>
2023-03-18 00:05:57 -0700

    Added algorithm in human language for first helper function

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,33 +1,38 @@
-letters = {A:9,
-           B:2,
-           C:2,
-           D:4,
-           E:12,
-           F:2,
-           G:3,
-           H:2,
-           I:9,
-           J:1,
-           K:1,
-           L:4,
-           M:2,
-           N:6,
-           0:8,
-           P:2,
-           Q:1,
-           R:6,
-           S:4,
-           T:6,
-           U:4,
-           V:2,
-           W:2,
-           X:1,
-           Y:2,
-           Z:1
-           }
+POOL_OF_LETTERS = {A:9,
+                   B:2,
+                   C:2,
+                   D:4,
+                   E:12,
+                   F:2,
+                   G:3,
+                   H:2,
+                   I:9,
+                   J:1,
+                   K:1,
+                   L:4,
+                   M:2,
+                   N:6,
+                   0:8,
+                   P:2,
+                   Q:1,
+                   R:6,
+                   S:4,
+                   T:6,
+                   U:4,
+                   V:2,
+                   W:2,
+                   X:1,
+                   Y:2,
+                   Z:1}
 
 def draw_letters():
-    list = []
+    #Copiar dict a una variable auxiliar
+    #Crear lista vacia
+    #Recorrer 10 veces y guardar una letra DISPONIBLE en la lista
+        #Para ver que este disponible revisemoas el valor de la llave de la letra a entregar
+        #Una vez entregada, debemos disminuir en 1 la cantidad de esa letra
+        #Si letra no esta disponible, buscar otra que si lo este (random)
+    #Una vez que la lista tenga 10 elementos, retornamos la lista.
 
     
     return list
