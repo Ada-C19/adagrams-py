a4655ef5014f5745eb368f2eeb0037e0ccf92dd1
Sel <selenetenorio27@hotmail.com>
2023-03-19 15:44:13 -0700

    Implementation of first helper function called: draw_letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,45 +1,51 @@
-POOL_OF_LETTERS = {"A":9,
-                   "B":2,
-                   "C":2,
-                   "D":4,
-                   "E":12,
-                   "F":2,
-                   "G":3,
-                   "H":2,
-                   "I":9,
-                   "J":1,
-                   "K":1,
-                   "L":4,
-                   "M":2,
-                   "N":6,
-                   "0":8,
-                   "P":2,
-                   "Q":1,
-                   "R":6,
-                   "S":4,
-                   "T":6,
-                   "U":4,
-                   "V":2,
-                   "W":2,
-                   "X":1,
-                   "Y":2,
-                   "Z":1}
+import random
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 
 def draw_letters():
-    #Copiar dict a una variable auxiliar
-    AUX_POOL_OF_LETTERS = POOL_OF_LETTERS.copy()
-    
-    #Crear lista vacia
-    list =[]
-    
-    #Recorrer 10 veces y guardar una letra DISPONIBLE en la lista
-        #Para ver que este disponible revisemoas el valor de la llave de la letra a entregar
-        #Una vez entregada, debemos disminuir en 1 la cantidad de esa letra
-        #Si letra no esta disponible, buscar otra que si lo este (random)
-    #Una vez que la lista tenga 10 elementos, retornamos la lista.
 
+    aux_letter_pool = LETTER_POOL.copy()
     
-    return list
+    available_letters_list = []
+
+    while len(available_letters_list) < 10:
+        numbers = (random.randint(65,90))
+        letters_for_user = chr(numbers)
+
+        if aux_letter_pool[letters_for_user] > 1:
+            available_letters_list.append(letters_for_user) 
+            aux_letter_pool[letters_for_user] -= 1
+
+    return available_letters_list
+
+
 
 def uses_available_letters(word, letter_bank):
     pass
@@ -49,3 +55,5 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+print(draw_letters())
