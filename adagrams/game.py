f6b281f1c43421086280d0c018569197f351f139
Gabby <gabrielleyoung10@gmail.com>
2023-03-19 16:11:11 -0500

    Create function that returns score for input word

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,6 @@
 import random 
 
+
 def draw_letters():
     letters = (["A"] * 9 + ["B"] * 2 + ["C"] * 2 + ["D"] * 4 + ["E"] * 12 
                + ["F"] * 2 + ["G"] * 3 + ["H"] * 2 + ["I"] * 9 + ["J"]
@@ -12,6 +13,7 @@ def draw_letters():
 
     return hand
 
+
 def uses_available_letters(word, letter_bank):
     word = word.upper()
     for char in word:
@@ -19,8 +21,26 @@ def uses_available_letters(word, letter_bank):
             return False 
     return True 
 
+
 def score_word(word):
-    pass
+    letter_points = {
+        "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, 
+        "L": 1, "N": 1, "R": 1, "S": 1, "T": 1, 
+        "D": 2, "G": 2, "B": 3, "C": 3, "M": 3, 
+        "P": 3, "F": 4, "H": 4, "V": 4, "W": 4, 
+        "Y": 4, "K": 5, "J": 8, "X": 8, "Q": 10, 
+        "Z": 10 
+    }
+    
+    if len(word) >= 7 and len(word) <=10:
+        score = 8 
+    else:
+        score = 0
+
+    for char in word:
+        score += letter_points[char.upper()]
+    return score
+
 
 def get_highest_word_score(word_list):
     pass
