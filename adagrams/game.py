270b40388bed900c1861606507a9b38b964ecd52
Samantha Hurtado <samanthahurtado@samanthas-mbp.lan>
2023-03-19 15:23:40 -0700

    first success wave 2

--- adagrams/game.py
+++ adagrams/game.py
@@ -17,41 +17,65 @@ def draw_letters():
 
     return hand
 
+def uses_available_letters(word, letter_bank):
+    uniform_word = word.upper()
+    uniform_letter_bank = [string.upper() for string in letter_bank]
+    word_dict = {}
+    letter_bank_dict = {}
+    for char in uniform_word:
+        if char not in word_dict:
+            word_dict[char] = 0
+        elif char in word_dict:
+            word_dict[char] += 1
+    
+    for letter in uniform_letter_bank:
+        if letter not in letter_bank_dict:
+            letter_bank_dict[letter] = 0
+        elif letter in letter_bank_dict:
+            letter_bank_dict[letter] += 1
+    
+    for char in word_dict:
+        if char not in letter_bank_dict:
+            return False
+        elif word_dict[char] > letter_bank_dict[char]:
+            return False
+
+    return True 
 
 
-# def score_word(word):
-#     score = 0
-#     uniform_case_word = word.upper()
-#     one_pointers = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]
-#     two_pointers = ["D", "G"]
-#     three_pointers = ["B", "C", "M", "P"]
-#     four_pointers = ["F", "H", "V", "W", "Y"]
-#     five_pointers = ["K"]
-#     eight_pointers = ["J", "X"]
-#     ten_pointers = ["Q", "Z"]
+def score_word(word):
+    score = 0
+    uniform_case_word = word.upper()
+    one_pointers = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]
+    two_pointers = ["D", "G"]
+    three_pointers = ["B", "C", "M", "P"]
+    four_pointers = ["F", "H", "V", "W", "Y"]
+    five_pointers = ["K"]
+    eight_pointers = ["J", "X"]
+    ten_pointers = ["Q", "Z"]
 
-#     for letter in uniform_case_word:
-#         if letter in one_pointers:
-#             score += 1
-#         if letter in two_pointers:
-#             score += 2
-#         if letter in three_pointers:
-#             score += 3
-#         if letter in four_pointers:
-#             score += 4
-#         if letter in five_pointers:
-#             score += 5
-#         if letter in eight_pointers:
-#             score += 8
-#         if letter in ten_pointers:
-#             score += 10
+    for letter in uniform_case_word:
+        if letter in one_pointers:
+            score += 1
+        if letter in two_pointers:
+            score += 2
+        if letter in three_pointers:
+            score += 3
+        if letter in four_pointers:
+            score += 4
+        if letter in five_pointers:
+            score += 5
+        if letter in eight_pointers:
+            score += 8
+        if letter in ten_pointers:
+            score += 10
         
-#     if len(word) >= 7:
-#             score += 8
+    if len(word) >= 7:
+            score += 8
 
 
 
-#     return score 
+    return score 
 
 # def get_highest_word_score(word_list):
 #     highest_word_score = ("", 0)
