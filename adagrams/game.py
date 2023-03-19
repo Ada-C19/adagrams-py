8ff9c3c877d9613796b95dd9a7560537f202d91e
Yulia S <startyupp@gmail.com>
2023-03-19 00:21:12 -0700

    Created letter_cost tuple with letters groups and their cost in score_word function. assigned cost =0.

--- adagrams/game.py
+++ adagrams/game.py
@@ -59,6 +59,18 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
+    
+    LETTER_COST = (
+        (('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T '), 1),
+        (('D', 'G'), 2),
+        (('B', 'C', 'M', 'P'), 3),
+        (('F', 'H', 'V', 'W', 'Y'), 4),
+        (('K'), 5),
+        (('J', 'X'), 8),
+        (('Q', 'Z'), 10)
+        )
+    cost = 0
+    
     pass
 
 def get_highest_word_score(word_list):
