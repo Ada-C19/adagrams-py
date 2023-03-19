a9fcfe9569f0224615416b43472bc0e896296437
halimAremu <halimat3000@gmail.com>
2023-03-19 05:41:38 -0500

    wave 3 completed

--- adagrams/game.py
+++ adagrams/game.py
@@ -9,6 +9,15 @@ letter_set7 = ["N","R","T"] * 6
 letter_set8 = ["O"]* 8
 letters_list = letter_set1 + letter_set2 + letter_set3 + letter_set4\
                + letter_set5 + letter_set6 + letter_set7 + letter_set8
+letter_score_dict ={}
+for letter in ["A", "E", "I", "O", "U","L", "N", "R","S","T"]:
+    letter_score_dict[letter] = 1
+dict_items = {"D":2,"G":2,"K":5,"J":8,"X":8,"Q":10,"Z":10}
+letter_score_dict.update(dict_items)
+for letter in ["B", "C", "M", "P"]:
+    letter_score_dict[letter]= 3
+for letter in ["F", "H", "V", "W", "Y"]:
+    letter_score_dict[letter]= 4
 def draw_letters():
    
    letter_hand = random.sample(letters_list, 10)
@@ -33,7 +42,13 @@ def uses_available_letters(word, letter_bank):
            
            
 def score_word(word):
-    pass
+    score = 0
+    for char in word:
+        score += letter_score_dict[char.upper()]
+    if len(word) >= 7:
+        score += 8
+
+    return score
 
 def get_highest_word_score(word_list):
     
