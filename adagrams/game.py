4524b4372e65ead6fc2f037b14a6145c8d64294f
April Zhang <aprilzh23@gmail.com>
2023-03-19 03:59:06 -0700

    wave4 passed test4

--- adagrams/game.py
+++ adagrams/game.py
@@ -108,28 +108,27 @@ def get_highest_word_score(word_list):
     max_score = max(word_score_dict.values())             # get the max score
     all_max_words = [k for k,v in word_score_dict.items() if v == max_score]  # list of all max score words
 
-
     # check how many winning words
     count_win = len(all_max_words)
     # conditional statements
     # if only one winning return winning
     if count_win == 1:
-        return (all_max_words, max_score)
+        return (all_max_words[0], max_score)
     else:                                      # if have multiple max score
         # check len10 when count_win > 1
         len_10_str = [string for string in all_max_words if len(string) == 10]
         # count how many len10 words
         count_len_10 = len(len_10_str)
-        # no len10
-        # check min len, if multi min, return first one
+        # if no len10
         if count_len_10 == 0:
+            # check min len, if multi min, return first one
             min_len = len(all_max_words[0])
             min_index = 0
             for i in range(1, len(all_max_words)):
                 if len(all_max_words[i]) < min_len:
                     min_len = len(all_max_words[i])
                     min_index = i
-                    return (all_max_words[min_index], max_score)
+            return (all_max_words[min_index], max_score)
         # have len10
         else:
             return (len_10_str[0], max_score)
