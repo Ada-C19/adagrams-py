e6b0f46abde633f883c463061590ee0630dc87fd
Tigist Woldearegai <tigistlina@gmail.com>
2023-03-19 21:47:57 -0500

    passes all tests in all waves

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,20 @@
 import random
 import string
-import copy
-import collections
 
+value_of_letters = {
+        
+        "A" : 1 , "B" : 3 , "C": 3 , 
+        "D" : 2 , "E": 1 , "F": 4 ,
+        "G" : 2 , "H" : 4, "I" : 1, 
+        "J" : 8 , "K" : 5 ,"L" : 1 , 
+        "M" : 3 , "N" : 1 , "O" : 1 , 
+        "P": 3, "Q" : 10 , "R" : 1 ,
+        "S" : 1 , "T" : 1 , "Q" : 10 ,
+        "U" : 1 , "V" : 4 , "W" : 4 , 
+        "X" : 8 , "Y" : 4 , "Z" : 10
+        
+        }
+    
 
 def draw_letters():
     
@@ -44,20 +56,6 @@ def uses_available_letters(word, letter_bank):
 def score_word(word):
     word = word.upper()
     score = 0
-    value_of_letters = {
-        
-        "A" : 1 , "B" : 3 , "C": 3 , 
-        "D" : 2 , "E": 1 , "F": 4 ,
-        "G" : 2 , "H" : 4, "I" : 1, 
-        "J" : 8 , "K" : 5 ,"L" : 1 , 
-        "M" : 3 , "N" : 1 , "O" : 1 , 
-        "P": 3, "Q" : 10 , "R" : 1 ,
-        "S" : 1 , "T" : 1 , "Q" : 10 ,
-        "U" : 1 , "V" : 4 , "W" : 4 , 
-        "X" : 8 , "Y" : 4 , "Z" : 10
-        
-        }
-    
     for letter in word:
         score += value_of_letters[letter]
     if len(word) >= 7:
@@ -65,4 +63,20 @@ def score_word(word):
     return score
 
 def get_highest_word_score(word_list):
-    pass
+    
+    winner_word = None
+    winner_score = 0
+    for word in word_list:
+        score = score_word(word)
+        if score > winner_score:
+            winner_word = word
+            winner_score = score
+        elif score == winner_score and len(word) == len(winner_word):   
+            continue
+        elif score == winner_score:
+            if len(word) < len(winner_word) and len(winner_word) !=10:
+                winner_word = word
+            elif len(word)==10:
+                winner_word = word
+    return (winner_word, winner_score)        
+        
