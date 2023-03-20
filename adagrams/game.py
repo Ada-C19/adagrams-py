a98486d90bc9e260b2888f0b8cef51758797074f
Whitney <whitneyshake@gmail.com>
2023-03-20 12:06:34 -0700

    Wave 02 completed, all tests passing

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,46 +1,78 @@
 import random
 
-LETTER_POOL = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
-}
+# LETTER_POOL = {
+#     'A': 9, 
+#     'B': 2, 
+#     'C': 2, 
+#     'D': 4, 
+#     'E': 12, 
+#     'F': 2, 
+#     'G': 3, 
+#     'H': 2, 
+#     'I': 9, 
+#     'J': 1, 
+#     'K': 1, 
+#     'L': 4, 
+#     'M': 2, 
+#     'N': 6, 
+#     'O': 8, 
+#     'P': 2, 
+#     'Q': 1, 
+#     'R': 6, 
+#     'S': 4, 
+#     'T': 6, 
+#     'U': 4, 
+#     'V': 2, 
+#     'W': 2, 
+#     'X': 1, 
+#     'Y': 2, 
+#     'Z': 1
+# }
 
-player_one = []
-pool_conversion = list(LETTER_POOL)
+# player_one = []
+# pool_conversion = list(LETTER_POOL)
 
-def draw_letters():
+# def draw_letters():
 
-    re_ordered_list = random.sample(pool_conversion, len(pool_conversion))
-    game_letters = re_ordered_list[:10]
-    return game_letters
+#     re_ordered_list = random.sample(pool_conversion, len(pool_conversion))
+#     game_letters = re_ordered_list[:10]
+#     return game_letters
 
 # ----------------------------------------------------------------------------
-# letter_bank = draw_letters()
-# def uses_available_letters(word, letter_bank):
+
+
+def uses_available_letters(word, letters):
+    matches = []
+    word = word.upper()
+    for letter in word:
+        if letter in letters:
+            if matches.count(letter) < letters.count(letter):
+                matches.append(letter)
+            elif matches.count(letter) == letters.count(letter):
+                continue
+    
+    if len(matches) == len(word):
+        return True
+    else:
+        return False
+
+    
+# matches = []
+
+# def uses_available_letters(word, letters):
+#     word = word.upper()
+#     for letter in word:
+#         if letter in letters:
+#             if matches.count(letter) < letters.count(letter):
+#                 matches.append(letter)
+#             elif matches.count(letter) == letters.count(letter):
+#                 continue
+#         else: continue
+
+#     if len(matches) == len(word):
+#         return True
+#     else:
+#         return False
 
 
 # def score_word(word):
