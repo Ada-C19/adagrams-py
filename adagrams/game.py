e9ce6e3c7b997d1d3bb5d6dfacfa298544fc2dce
Charlie <charliethemushi@gmail.com>
2023-03-19 19:07:53 -0700

    built the dictionary

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,4 +1,48 @@
-def draw_letters():
+import random
+    #Your first task is to build a hand of 10 letters for the user. To do so, implement the function draw_letters in game.py. This method should have the following properties:
+"""
+No parameters
+Returns an array of ten strings
+Each string should contain exactly one letter
+These represent the hand of letters that the player has drawn
+The letters should be randomly drawn from a pool of letters
+This letter pool should reflect the distribution of letters as described in the table below
+There are only 2 available C letters, so draw_letters cannot ever return more than 2 Cs -- no sets then, look for case sensativity
+Since there are 12 Es but only 1 Z, it should be 12 times as likely for the user to draw an E as a Z
+Invoking this function should not change the pool of letters
+Imagine that the user returns their hand to the pool before drawing new letters
+
+"""
+#in letter out qty
+letter_dict = {
+"A" : 9,	
+"B" : 2,
+"C" : 2,	
+"D" : 4,	
+"E" : 12,	
+"F" : 2,	
+"G" : 3,	
+"H" : 2,	
+"I" : 9,
+"J" : 1,	
+"K" : 1,	
+"L" : 4,
+"M" : 2,
+"O": 8,
+"P": 2,
+"Q": 1,
+"R": 6,
+"S": 4,
+"T": 6,
+"U": 4,
+"V": 2,
+"W": 2,
+"X": 1,
+"Y": 2,
+"Z": 1,
+}
+
+
     pass
 
 def uses_available_letters(word, letter_bank):
