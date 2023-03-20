c741bc20ea340cfbd9640dcf44f291485a20843a
Tigist Woldearegai <tigistlina@gmail.com>
2023-03-19 19:23:44 -0500

    done with wave3 and passed all tests.

--- adagrams/game.py
+++ adagrams/game.py
@@ -41,9 +41,28 @@ def uses_available_letters(word, letter_bank):
     return True    
         
             
-
 def score_word(word):
-    pass
+    word = word.upper()
+    score = 0
+    value_of_letters = {
+        
+        "A" : 1 , "B" : 3 , "C": 3 , 
+        "D" : 2 , "E": 1 , "F": 4 ,
+        "G" : 2 , "H" : 4, "I" : 1, 
+        "J" : 8 , "K" : 5 ,"L" : 1 , 
+        "M" : 3 , "N" : 1 , "O" : 1 , 
+        "P": 3, "Q" : 10 , "R" : 1 ,
+        "S" : 1 , "T" : 1 , "Q" : 10 ,
+        "U" : 1 , "V" : 4 , "W" : 4 , 
+        "X" : 8 , "Y" : 4 , "Z" : 10
+        
+        }
+    
+    for letter in word:
+        score += value_of_letters[letter]
+    if len(word) >= 7:
+        score += 8     
+    return score
 
 def get_highest_word_score(word_list):
     pass
