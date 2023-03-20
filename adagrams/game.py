a785528954ec5789da051ea3c80c1339d7e29e10
Jackie <jackiemay08@gmail.com>
2023-03-20 09:26:53 -0400

    third wave commit. the function scores a word correctly.

--- adagrams/game.py
+++ adagrams/game.py
@@ -44,11 +44,9 @@ def draw_letters():
 def uses_available_letters(word, letter_bank):
     letters_used = [letter for letter in letter_bank]
     word = word.upper()
-    print(word)
     for letter in word:
         if letter in letters_used:
             letters_used.remove(letter)
-    print(letter_bank, letters_used, word)
     if len(word) == len(letter_bank) - len(letters_used):
         return True
     else:
@@ -56,7 +54,45 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    letter_points = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+    }
+    
+    word = word.upper()
+    score = int()
+    for letter in word:
+        if letter in letter_points:
+            score += letter_points[letter]
+    if len(word) >=7 and len(word) <=10:
+        score += 8
+    
+    return score
+
 
 def get_highest_word_score(word_list):
     pass
