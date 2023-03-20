55af4b244bdf754cb30f866996abdebb303e602b
Johanna Cabanban <johanna.cabanban@gmail.com>
2023-03-19 20:35:44 -0400

    Passed all wave 3 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -29,6 +29,35 @@ LETTER_POOL = {
     'Z': 1
 }
 
+letter_values = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+}
+
 def draw_letters():
     # assign empty array to variable hand
     hand = []
@@ -83,7 +112,20 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    # assign int to variable for the score
+    score = 0
+    letter_list = list(word.upper())
+
+    # create a loop to add values of each letter to score
+    for letter in letter_list:
+        score += letter_values[letter]
+
+    # check length of word for bonus point 
+    if len(letter_list) >= 7:
+        score += 8
+
+    # return score
+    return score
 
 def get_highest_word_score(word_list):
     pass
