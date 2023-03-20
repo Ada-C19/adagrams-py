05d7a7cf0b9d84616a77811a70aa17bbec0239a2
Samantha Hurtado <samanthahurtado@Samanthas-MacBook-Pro.local>
2023-03-20 15:54:22 -0700

    added some comments for clarity

--- adagrams/game.py
+++ adagrams/game.py
@@ -4,11 +4,11 @@ import random
 
 def draw_letters():
     letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    relative_weights = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1]
+    relative_weights = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1] #how many of each letter in pool of letters
     weighted_list = []
     hand = []
     for i in range(len(letters)):
-        weighted_list.extend(letters[i] * relative_weights[i])
+        weighted_list.extend(letters[i] * relative_weights[i]) #extended list with right number of each letter
     
     while len(hand) < 10:
         random_letter = random.choice(weighted_list)
@@ -44,30 +44,17 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    score = 0
-    uniform_case_word = word.upper()
-
-    point_dict = {
-        ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T") : 1,
-        ("D", "G"): 2,
-        ("B", "C", "M", "P") : 3,
-        ("F", "H", "V", "W", "Y") : 4,
-        ("K") : 5,
-        ("J", "X") : 8,
-        ("Q", "Z") : 10
+    letter_scores = {
+        "A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I": 1, "J": 8, "K": 5, "L": 1, "M": 3,
+        "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10
     }
     
-
-    for letter in uniform_case_word:
-        for tuple in point_dict:
-            if letter in tuple:
-                score += point_dict[tuple]
-        
-    if len(word) >= 7:
-            score += 8
-
-
-    return score 
+    score = 0
+    for letter in word:
+        score += letter_scores[letter.upper()]
+    if len(word) in [7, 8, 9, 10]:
+        score += 8
+    return score
 
 def get_highest_word_score(word_list):
 
@@ -76,7 +63,7 @@ def get_highest_word_score(word_list):
 
     for word in word_list:
         score = score_word(word)
-        print(word + " " + str(score))
+        # print(word + " " + str(score)) # for debugging
         if score > winning_score:
             winning_score = score
             tied_words = [word] 
