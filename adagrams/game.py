2a43dc1fc8ace3289d609b9c8780213ee28db7fc
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-03-20 17:56:24 -0400

    added clarifying comments

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,39 +1,40 @@
 from random import choice
 
+
 def draw_letters():
     LETTER_POOL = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
-}
+        'A': 9,
+        'B': 2,
+        'C': 2,
+        'D': 4,
+        'E': 12,
+        'F': 2,
+        'G': 3,
+        'H': 2,
+        'I': 9,
+        'J': 1,
+        'K': 1,
+        'L': 4,
+        'M': 2,
+        'N': 6,
+        'O': 8,
+        'P': 2,
+        'Q': 1,
+        'R': 6,
+        'S': 4,
+        'T': 6,
+        'U': 4,
+        'V': 2,
+        'W': 2,
+        'X': 1,
+        'Y': 2,
+        'Z': 1
+    }
 
-    #Creating the player's "hand" and putting it into a letters list
+    # Creating the player's "hand" and putting it into a letters list
     letters = []
     while len(letters) < 10:
-        #choose a random letter from a list of LETTER_POOL keys
+        # choose a random letter from a list of LETTER_POOL keys
         rand_letter = choice(list(LETTER_POOL.keys()))
         if letters.count(rand_letter) >= LETTER_POOL[rand_letter]:
             continue
@@ -44,22 +45,23 @@ def draw_letters():
 
 
 def uses_available_letters(word, letter_bank):
-    word = word.upper() #forces the input to be case insensitive
+    word = word.upper()  # forces the input to be case insensitive
 
-    #to ensure each letter is in letter_bank and does not occur too frequently:
+    # to ensure each letter is in letter_bank and does not occur too frequently:
     for letter in word:
         if letter not in letter_bank or word.count(letter) > letter_bank.count(letter):
             return False
     return True
 
+
 def score_word(word):
-    #minimum required length of word to qualify for a long word bonus:
+    # minimum required length of word to qualify for a long word bonus:
     BONUS_POINT_MIN_LENGTH = 7
-    #bonus points to be awarded for words that meet the bonus minimum length:
+    # bonus points to be awarded for words that meet the bonus minimum length:
     BONUS_POINTS = 8
 
     word = word.upper()
-    
+
     letter_values = {
         'AEIOULNRST': 1,
         'DG': 2,
@@ -69,7 +71,7 @@ def score_word(word):
         'JX': 8,
         'QZ': 10
     }
-    
+
     score = 0
     if word:
         if len(word) >= BONUS_POINT_MIN_LENGTH:
@@ -83,23 +85,26 @@ def score_word(word):
 
 
 def get_highest_word_score(word_list):
-    MAX_LETTER_LENGTH = 10  
+    MAX_LETTER_LENGTH = 10
     word_scores = {}
     high_score = 0
 
+    # put the words from word_list into word_scores dict with their scores
     for word in word_list:
         score = score_word(word)
-        word_scores[word] = score #put the word into word_scores with its score
+        word_scores[word] = score
         if score > high_score:
-            high_score = score #keep track of the high score to find which words match it
+            high_score = score  # keep track of the high score to find which words match it
 
-    highest_scoring_words = {key:value for key,value in word_scores.items() if word_scores[key] == high_score}
+    highest_scoring_words = {key: value for key, value in word_scores.items(
+    ) if word_scores[key] == high_score}
 
     if len(highest_scoring_words) > 1:
 
         words = [word for word in highest_scoring_words]
         biggest_word = max(words, key=lambda word: len(word))
-        smallest_word = min(words, key=lambda word: len(word))
+        smallest_word = min(words, key=lambda word: len(word)) #will return the first smallest word
+        # in the event of multiple small words w/ same length
 
         if len(biggest_word) == MAX_LETTER_LENGTH:
             return (biggest_word, highest_scoring_words[biggest_word])
