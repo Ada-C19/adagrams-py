e093e96818ce84feb2c0e8f83ba615a4cd3b4bcc
Whitney <whitneyshake@gmail.com>
2023-03-20 22:20:44 -0700

    Wave 03 of Adagrams completed, all tests passed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,42 +1,42 @@
 import random
 
-# LETTER_POOL = {
-#     'A': 9, 
-#     'B': 2, 
-#     'C': 2, 
-#     'D': 4, 
-#     'E': 12, 
-#     'F': 2, 
-#     'G': 3, 
-#     'H': 2, 
-#     'I': 9, 
-#     'J': 1, 
-#     'K': 1, 
-#     'L': 4, 
-#     'M': 2, 
-#     'N': 6, 
-#     'O': 8, 
-#     'P': 2, 
-#     'Q': 1, 
-#     'R': 6, 
-#     'S': 4, 
-#     'T': 6, 
-#     'U': 4, 
-#     'V': 2, 
-#     'W': 2, 
-#     'X': 1, 
-#     'Y': 2, 
-#     'Z': 1
-# }
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 
-# player_one = []
-# pool_conversion = list(LETTER_POOL)
+player_one = []
+pool_conversion = list(LETTER_POOL)
 
-# def draw_letters():
+def draw_letters():
 
-#     re_ordered_list = random.sample(pool_conversion, len(pool_conversion))
-#     game_letters = re_ordered_list[:10]
-#     return game_letters
+    re_ordered_list = random.sample(pool_conversion, len(pool_conversion))
+    game_letters = re_ordered_list[:10]
+    return game_letters
 
 # ----------------------------------------------------------------------------
 
@@ -56,27 +56,46 @@ def uses_available_letters(word, letters):
     else:
         return False
 
-    
-# matches = []
+# ---------------------------------------------------------------
 
-# def uses_available_letters(word, letters):
-#     word = word.upper()
-#     for letter in word:
-#         if letter in letters:
-#             if matches.count(letter) < letters.count(letter):
-#                 matches.append(letter)
-#             elif matches.count(letter) == letters.count(letter):
-#                 continue
-#         else: continue
+points_dict = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+}
+def score_word(word):
 
-#     if len(matches) == len(word):
-#         return True
-#     else:
-#         return False
+    score = 0
+    for letter in word.upper():
+        score += points_dict[letter]
+    if len(word) >= 7 <= 10:
+        score += 8
+        
 
-
-# def score_word(word):
-    # pass
+    return score
 
 # def get_highest_word_score(word_list):
     # pass
