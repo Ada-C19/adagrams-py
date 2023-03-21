fd327f66aa60c29c8f74ff2f42036ddddf44b058
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-03-21 14:46:51 -0700

    first test works and gives me 10 letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,13 +1,43 @@
+import random 
+import string
 
-import random
-import string 
-
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 def draw_letters():
-    hand_count = []
-    randomLetter = random.choice(string.ascii_letters)
-    while len(hand_count) < 10:
-        hand_count.append(randomLetter)
-    print(hand_count)
+    drawn_letters = []
+    
+    
+    while len(drawn_letters) < 10:
+        randomLetter = random.choice(string.ascii_uppercase)
+        drawn_letters += randomLetter
+            
+    print(len(drawn_letters))
     
 
     ten_letters = ["A","Z","O","P","E","T","Q","W","D","L"]
