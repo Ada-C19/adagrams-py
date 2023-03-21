06c27483e751eb9146e8e8c1d5402f3533f16a9c
Divya <divya.naidoo5@gmail.com>
2023-03-21 14:31:25 -0700

    got wave 1 to work

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,17 +1,62 @@
 import random
 
 def draw_letters():
-    LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    random.choices(LETTERS, weights=(9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1), k=10)
-    
+    # LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    # "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    # letter_bank = random.choices(LETTERS, weights=(9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1), k=10)
+
+    # print(letter_bank)
+    # return letter_bank
+
+    LETTER_POOL = {
+        'A': 9, 
+        'B': 2, 
+        'C': 2, 
+        'D': 4, 
+        'E': 12, 
+        'F': 2, 
+        'G': 3, 
+        'H': 2, 
+        'I': 9, 
+        'J': 1, 
+        'K': 1, 
+        'L': 4, 
+        'M': 2, 
+        'N': 6, 
+        'O': 8, 
+        'P': 2, 
+        'Q': 1, 
+        'R': 6, 
+        'S': 4, 
+        'T': 6, 
+        'U': 4, 
+        'V': 2, 
+        'W': 2, 
+        'X': 1, 
+        'Y': 2, 
+        'Z': 1
+    }
+
     letter_bank = []
-    letter = random.choices(LETTERS, weights=(9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1), k=10)
-    letter_bank.append(letter)
+    for i in range(10):
+        letter = random.choice(list(LETTER_POOL.keys()))
+        letter_bank.append(letter)
+        LETTER_POOL[letter] -= 1
+        
+        if LETTER_POOL[letter] == 0:
+            LETTER_POOL.pop(letter)
+
 
     print(letter_bank)
+    return letter_bank
+
+        # decrease letter frequency in dictionary
+        # if frequency is 0 then remove key from dictionary
+
+
 
 draw_letters()
+
 
 def uses_available_letters(word, letter_bank):
     pass
