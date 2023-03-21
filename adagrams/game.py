6101b2f641a09c3d00fe502a9868736efaf14ceb
Agnes Kim <agneskim4@gmail.com>
2023-03-21 19:41:53 -0400

    refactored the last code to shortened in wave 4

--- adagrams/game.py
+++ adagrams/game.py
@@ -141,21 +141,17 @@ def get_highest_word_score(word_list):
 #     - If the there are multiple words that are the same score and the same length, pick the first one in the supplied list
 
 # FUNCTION BODY
-    all_scores = {}
     top_word = ""
     top_score = 0
-    count_ten_words = 0
+    
     for word in word_list:
         score = score_word(word)
         if score > top_score:
-            if len(word) == 10:
-                count_ten_words +=1
             top_score = score
             top_word = word
         elif score == top_score:
-            if len(word) ==10 and count_ten_words <= 0:
+            if len(word) ==10 and len(top_word) !=10:
                 top_word = word
-                count_ten_words +=1
             elif len(word) < len(top_word) and len(top_word) != 10:
                 top_word = word
                 
