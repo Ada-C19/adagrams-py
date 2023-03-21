c228861e237ee30415c592e4f53ae232040bb95c
Rediet Medhane <redietkmedhane@gmail.com>
2023-03-20 17:58:39 -0700

    passes wave 3!

--- adagrams/game.py
+++ adagrams/game.py
@@ -27,6 +27,34 @@ LETTER_QUANTITY = {
     'Y': 2, 
     'Z': 1
 }  
+LETTER_POINT_SYSTEM = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+    }
 
 def draw_letters():
     # creates a list of ten randomized characters
@@ -80,7 +108,7 @@ def uses_available_letters(word, letter_bank):
         else:
             letter_frequency[char] = 1   
 
-        # my question/where i am stuck!! 
+
         if letter_frequency[char]> max_frequency[char]:
             valid_frequency = False
             valid_word = False
@@ -89,17 +117,48 @@ def uses_available_letters(word, letter_bank):
 
     return valid_word
 
-#  taken from test files:  
-#       letter_freq = {}
-#         for letter in letters:
-#             if letter in letter_freq:
-#                 letter_freq[letter] += 1
-#                 else:
-#                     letter_freq[letter] = 1
-#     return valid_word
 
 def score_word(word):
-    pass
+    #takes in the valid word and returns an intger representing points
+    #points calculated by adding up each letter's point values
+    #step one: irtate through each character and += each point value
+    #step two: check if word length is 7-10 characters and add 8 extra points if so
+    point_total = 0
+    for char in word:
+        char = char.capitalize()
+        point_total += LETTER_POINT_SYSTEM[char]
+    if len(word) >= 7:
+        point_total += 8    
+    return point_total    
+
+
 
 def get_highest_word_score(word_list):
-    pass
+    #takes in word_list, a list of strings
+    #calculate score of each word in list
+    #in the event of a tie: prefer word with fewest letters OR 10 letters
+    #if same score and length, pick the first one to occur
+    #return tuple with (string word, score)
+
+    # LIST METHOD
+    points_list = []
+    for word in word_list:
+        points_list.append(score_word(word))
+    for i in len(points_list):
+        top_score = max(points_list)
+        
+
+       
+
+    # DICTIONARY METHOD
+    # word_score_log = {}
+    # for word in word_list:
+    #     word_score_log[word] = score_word(word)
+    # top_score = max(word_score_log.values()) 
+    # print(top_score) 
+    # top_score_word = 
+
+    # best_word = (word_score_log.values().index(top_score), top_score)  
+
+
+
