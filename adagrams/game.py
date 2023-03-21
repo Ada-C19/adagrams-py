4c7f2828440db35b4a60e3de0cf46ce8df56254d
alyssa <tn.alyssa@gmail.com>
2023-03-21 16:27:36 -0500

    optimized get_highest_word_score using sorted() and tuple()

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,83 +1,69 @@
 import random
 
 def generate_letter_pool():
+    """Return a list of all the available letter tiles."""
     LETTER_POOL = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
+        'A': 9,
+        'B': 2, 
+        'C': 2, 
+        'D': 4, 
+        'E': 12, 
+        'F': 2, 
+        'G': 3, 
+        'H': 2, 
+        'I': 9, 
+        'J': 1, 
+        'K': 1, 
+        'L': 4, 
+        'M': 2, 
+        'N': 6, 
+        'O': 8, 
+        'P': 2, 
+        'Q': 1, 
+        'R': 6, 
+        'S': 4, 
+        'T': 6, 
+        'U': 4, 
+        'V': 2, 
+        'W': 2, 
+        'X': 1, 
+        'Y': 2, 
+        'Z': 1
     }
     letter_pool_list = []
     for letter, quantity in LETTER_POOL.items():
         letter_pool_list.extend([letter] * quantity)
-    
-    return letter_pool_list 
-
-print(generate_letter_pool())
+    return letter_pool_list
 
 
-def casefold_letters(letters):
+def casefold_letters(word):
+    """Return a list letters from a word in all lowercase."""
     casefolded_letters = []
-    for letter in letters:
+    for letter in word:
         casefolded_letters.append(letter.casefold())
     
     return casefolded_letters
 
 
-def count_available_letters(letters):
+def count_available_letters(letter_bank):
+    """Return a dictionary containing the quantity of each letter in letter_bank."""
     letter_counts = {}
-    for letter in letters:
-        letter_counts[letter] = 0
-    for letter in letters:
+    # for letter in letter_bank:
+    #     letter_counts[letter] = 0
+    # for letter in letter_bank:
+    #     letter_counts[letter] += 1
+
+    for letter in letter_bank:
+        if letter not in letter_counts:
+            letter_counts[letter] = 0
         letter_counts[letter] += 1
     
     return letter_counts
 
 
-def find_top_word_and_score(scores):
-    top_word = ''
-    top_score = 0
-
-    for word, score in scores.items():
-        if score > top_score:
-            top_word = word
-            top_score = score
-        elif score == top_score:
-            if len(word) == 10 and len(top_word) != 10:
-                top_word = word
-                top_score = score
-            elif len(top_word) == 10:
-                continue
-            elif len(word) < len(top_word):
-                top_word = word
-                top_score = score
-    
-    return top_word, top_score
-
 
 def draw_letters():
+    """Return a list of 10 randomly chosen letters."""
     letter_pool_list = generate_letter_pool()
     letter_bank = []
 
@@ -90,6 +76,7 @@ def draw_letters():
 
 
 def uses_available_letters(word, letter_bank):
+    """Return true if a word uses only the letters from letter_bank."""
     letter_bank = casefold_letters(letter_bank)
     word = casefold_letters(word)
     letter_counts = count_available_letters(letter_bank)
@@ -105,32 +92,32 @@ def uses_available_letters(word, letter_bank):
 
 def score_word(word):
     point_values = {
-    'A': 1, 
-    'B': 3, 
-    'C': 3, 
-    'D': 2, 
-    'E': 1, 
-    'F': 4, 
-    'G': 2, 
-    'H': 4, 
-    'I': 1, 
-    'J': 8, 
-    'K': 5, 
-    'L': 1, 
-    'M': 3, 
-    'N': 1, 
-    'O': 1, 
-    'P': 3, 
-    'Q': 10, 
-    'R': 1, 
-    'S': 1, 
-    'T': 1, 
-    'U': 1, 
-    'V': 4, 
-    'W': 4, 
-    'X': 8, 
-    'Y': 4, 
-    'Z': 10
+        'A': 1, 
+        'B': 3, 
+        'C': 3, 
+        'D': 2, 
+        'E': 1, 
+        'F': 4, 
+        'G': 2, 
+        'H': 4, 
+        'I': 1, 
+        'J': 8, 
+        'K': 5, 
+        'L': 1, 
+        'M': 3, 
+        'N': 1, 
+        'O': 1, 
+        'P': 3, 
+        'Q': 10, 
+        'R': 1, 
+        'S': 1, 
+        'T': 1, 
+        'U': 1, 
+        'V': 4, 
+        'W': 4, 
+        'X': 8, 
+        'Y': 4, 
+        'Z': 10
     }
 
     score = 0
@@ -144,25 +131,28 @@ def score_word(word):
         
 def get_highest_word_score(word_list):
     scores = {}
-    potential_winning_word = ""
-    second_potential_winning_word = ""
-    potential_highest_score = 0
-    second_potential_highest_score = 0
+    winner = ("", 0)
 
     for word in word_list:
         scores[word] = score_word(word)
+    
+    sorted_scores = sorted(scores.items())
+    
+    for score in range(len(sorted_scores)):
+        comparator_word = sorted_scores[score][0]
+        comparator_points = sorted_scores[score][1]
+        if comparator_points > winner[1]:
+            winner = sorted_scores[score]
+        elif comparator_points == winner[1]:
+            if len(winner[0]) == 10:
+                continue
+            elif len(comparator_word) == 10:
+                winner = sorted_scores[score]
+            elif len(winner[0]) < len(comparator_word):
+                continue
+            elif len(comparator_word) < len(winner[0]):
+                winner = sorted_scores[score]
 
-    potential_winning_word, potential_highest_score = find_top_word_and_score(scores)
-        
-    scores.pop(potential_winning_word)
+    return winner
 
-    second_potential_winning_word, second_potential_winning_score = find_top_word_and_score(scores)
-
-    top_two_words = {
-        potential_winning_word : potential_highest_score,
-        second_potential_winning_word : second_potential_winning_score
-    }
-
-    winning_word_and_score = find_top_word_and_score(top_two_words)
-
-    return winning_word_and_score
+print(get_highest_word_score(["WWW", "MMMM", "BBBBBB", "AAAAAAAAD", "JQ", "KFHK"]))
