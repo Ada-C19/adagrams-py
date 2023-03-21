60f55c933fb639435fd51d29bf1dcf17230d42d5
Monica <monicalagdaan@hotmail.com>
2023-03-20 21:33:32 -0700

    Draw letters is working

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,47 @@
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 def draw_letters():
-    pass
+    used_letters = {}
+    letters = []
+    possible_letters = list(LETTER_POOL.keys())
+
+    while len(letters) <= 9:
+        random_int = random.randint(0, 25)
+        random_l =  possible_letters[random_int]
+        if not used_letters.get(random_l) == LETTER_POOL[random_l]:
+            if (not used_letters.get(random_l)):
+                used_letters[random_l] = 0 
+            used_letters[random_l] += 1
+            letters.append(random_l)
+        
+    return letters
 
 def uses_available_letters(word, letter_bank):
     pass
