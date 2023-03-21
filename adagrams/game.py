5f745b09bb16fe0768ed59d61797972fe36cc859
Zhuo <zhuo.parse@gmail.com>
2023-03-21 12:00:41 -0400

    Modified def draw_letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,6 +1,46 @@
+import random
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 def draw_letters():
-    pass
-
+    draw_letters_list = []
+    letters_freq_dic = {}
+    while len(draw_letters_list) < 10:
+        cur_random_letter = random.choice(list(LETTER_POOL.keys()))
+        if cur_random_letter in letters_freq_dic.keys():
+            letters_freq_dic[cur_random_letter] += 1
+        else:
+            letters_freq_dic[cur_random_letter] = 1
+        if letters_freq_dic[cur_random_letter] > LETTER_POOL[cur_random_letter]:
+            continue
+        else:
+            draw_letters_list. append(cur_random_letter)
+    return draw_letters_list
 def uses_available_letters(word, letter_bank):
     pass
 
