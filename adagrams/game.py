ffde27aca79dba1546aeacf11ef1c4a7c86fc8f0
Ana Silvera <anasilverx@gmail.com>
2023-03-21 13:55:35 -0700

    Added comments to code for clarity

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,7 @@
 import random 
-
+"""
+Define a list of tuples, to access frequency of letter and letter at same index
+"""
 LETTER_POOL = [
     ("A", 9),
     ("B", 2),
@@ -30,6 +32,12 @@ LETTER_POOL = [
 ]
 
 def draw_letters():
+    """
+    Create a list number_of_letter_available of same lenght as LETTER_POOL
+    Generate random integer, to get random index to access number_of_letters_available
+    If number is greater than one, add to letter_bank list and subtract one, at the given index
+    Returns letter_bank
+    """
     letter_bank = []
     number_of_letter_available = []
     
@@ -46,10 +54,16 @@ def draw_letters():
 
 
 def uses_available_letters(word, letter_bank):
+    """
+    Create a dictionary looping over letter in letter_bank 
+    Create key using letter, sum quantities of given letter key
+    Create dictionary for letter in word, similarly 
+    Returns True or False
+    """
     cap_word = word.upper()
     letter_bank_quantity = {}
     char_quantity = {}
-    result = None
+    result = None #flag called result 
 
     for letter in letter_bank:
         if letter not in letter_bank_quantity:
@@ -73,6 +87,12 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
+    """
+    Define a list of tuples with group of letter's point value
+    Loop over letter_score list to find if each character in word is in list
+    If char is found in that index, score will be same index of outer list, index 1 of inner list
+    Returns sum_score, holds total points for given word 
+    """
     letter_score = [
         [("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"), 1],
         [("D", "G"), 2],
@@ -88,8 +108,8 @@ def score_word(word):
 
     for char in cap_word:
         for i in range(len(letter_score)):
-            if char in letter_score[i][0]:
-                sum_score += letter_score[i][1]
+            if char in letter_score[i][0]: #check if char is in list of letters in inner list index 0
+                sum_score += letter_score[i][1] #sums integer in inner list index 1
     
     if len(word) >= 7:
         sum_score += 8
@@ -98,11 +118,16 @@ def score_word(word):
 
 
 def get_highest_word_score(word_list):
-    
+    """
+    Create a list of tuples containing word from word_list and score 
+    Calculate score calling score_word function for word
+    Each word and corresponding score will be at same index of list 
+    Returns tuple of highest scoring word and corresponding score
+    """
     words_and_score = []
-    index_of_highest_tuple = 0
     
-    highest_score = 0
+    index_of_highest_tuple = 0 #keeps track of index where the current highest scoring word is located
+    highest_score = 0 
 
     for word in word_list:
         score = score_word(word)
@@ -114,16 +139,15 @@ def get_highest_word_score(word_list):
         word = words_and_score[i][0]
         if score > highest_score:
             highest_score = score
-            index_of_highest_tuple = i
+            index_of_highest_tuple = i #assigns index of word currently being checked in for loop
         elif score == highest_score:
             current_highest = words_and_score[index_of_highest_tuple][0]
-            if len(word) == len(current_highest):
-                #index_of_highest_tuple stays the same
-                continue
+            if len(word) == len(current_highest):    
+                continue #index_of_highest_tuple stays the same
             elif len(current_highest) == 10:
-                continue
+                continue 
             elif len(word) < len(current_highest):
-                index_of_highest_tuple = i
+                index_of_highest_tuple = i #updates index_of_highest_tuple 
             elif len(word) == 10:
                 index_of_highest_tuple = i
                                 
