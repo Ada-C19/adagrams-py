919d5c5d4fa4913f13170ba9e4257217723fd150
Ann <anntuminello@gmail.com>
2023-03-21 15:47:53 -0700

    Wave 1 and I think wave 2 commit

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,37 @@
+import random
+
 def draw_letters():
-    pass
+    letter_pool = {'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1}
+    hand = []
+    for i in range(10):
+        letters = list(letter_pool.keys())
+        letter = random.choice(letters)
+        while letter_pool[letter] == 0:
+            letter = random.choice(letters)
+        hand.append(letter)
+        letter_pool[letter] -= 1
+    return hand
+
 
 def uses_available_letters(word, letter_bank):
-    pass
+    
+    letter_bank_copy = letter_bank[]
+    
+    
+    word_lower = word.lower()
+    letter_bank_lower = [letter.lower() for letter in letter_bank_copy]
+    
+    for letter in word_lower:
+        if letter in letter_bank_lower:
+            letter_bank_lower.remove(letter)
+        else:
+            return False
+    return True
+
+
+
+
+
 
 def score_word(word):
     pass
