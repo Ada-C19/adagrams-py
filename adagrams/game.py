687be9b4fe862dbbee434b720d6c9b413c19f71e
nadia <reach.nadia.saria@gmail.com>
2023-03-21 16:03:50 -0700

    Wave 03

--- adagrams/game.py
+++ adagrams/game.py
@@ -55,7 +55,28 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    word = word.upper()
+    score = 0
+    for letter in word:
+        if letter in ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]:
+            score += 1
+        elif letter in ["D", "G"]:
+            score += 2
+        elif letter in ["B", "C", "M", "P"]:
+            score += 3
+        elif letter in ["F", "H", "V", "W", "Y"]:
+            score += 4
+        elif letter == "K":
+            score += 5
+        elif letter in ["J", "X"]:
+            score += 8
+        elif letter in ["Q", "Z"]:
+            score += 10
+        
+    if len(word) >= 7:
+        score += 8
+
+    return score
 
 def get_highest_word_score(word_list):
     pass
