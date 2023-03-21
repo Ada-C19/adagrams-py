a5e6478c2c972e3537c794ee1fe66d23f4c1a610
jasmine <jtmsoto@gmail.com>
2023-03-21 13:39:56 -0700

    Add comments to explain code

--- adagrams/game.py
+++ adagrams/game.py
@@ -33,24 +33,24 @@ TOTAL_TILES_PER_HAND = 10
 
 def draw_letters():
     letter_bank = []
+    player_hand = []
 
+    # Generates a letter bank based on the letter frequency stored in LETTER_TILES
     for letter in LETTER_TILES:
         for i in range(LETTER_TILES[letter]['frequency']):
             letter_bank.append(letter)
 
-    player_hand = []
-
     while len(player_hand) < TOTAL_TILES_PER_HAND:
         current_letter = random.choice(letter_bank)
         letter_occurences = 0
-        
+
+        # Checks how many times a letter occurs in player_hand and skips adding
+        # if max frequency is hit
         if current_letter in player_hand:
             for current_letter in player_hand:
                 letter_occurences += 1
-        
         if letter_occurences >= LETTER_TILES[current_letter]['frequency']:
                 continue
-
         player_hand.append(current_letter)
 
     return player_hand
@@ -59,6 +59,8 @@ def uses_available_letters(word, letter_bank):
     letter_bank_copy = letter_bank.copy()
     word = word.upper()
 
+    # Uses letter_bank_copy to compare word against, removing letters that
+    # have been used already.
     for letter in word:
         if letter not in letter_bank_copy:
             return False
@@ -68,8 +70,12 @@ def uses_available_letters(word, letter_bank):
 def score_word(word):
     score = 0
     word = word.upper()
+
     for letter in word:
         score += LETTER_TILES[letter]['value']
+
+    # Checks for words between 7-10 letters and adds bonus 8 points
+    # if condition is true
     if 7 <= len(word) <= TOTAL_TILES_PER_HAND:
         score += 8
     return score
@@ -80,11 +86,14 @@ def get_highest_word_score(word_list):
     best_word = None
     best_score = 0
 
+    # Creates a library of words with their score for comparison
     for word in word_list:
         word_score = score_word(word)
         all_scores[word] = word_score
 
     for word, score in all_scores.items():
+        # First 10-letter word should be the top scoring word regardless
+        # of other words inputted
         if len(word) == TOTAL_TILES_PER_HAND:
             best_word = word
             best_score = score
@@ -92,6 +101,7 @@ def get_highest_word_score(word_list):
         if score > best_score:
             best_word = word
             best_score = score
+        # If two words are the same score, the shorter word should be prioritised
         if score == best_score:
             if len(word) < len(best_word):
                 best_word = word
