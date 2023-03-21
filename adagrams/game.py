be4d6868ef465f7d20b3e53f0d2cce524f5bf315
Sel <selenetenorio27@hotmail.com>
2023-03-20 23:22:21 -0700

    Modified input variable word to uppercase in helper function called: uses_available_letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -48,7 +48,7 @@ def draw_letters():
 
 
 def uses_available_letters(word, letter_bank):
-    
+    word = word.upper()
     if len(letter_bank) < len(word):
         return False
     aux_letter_bank = letter_bank.copy()
@@ -103,34 +103,26 @@ def score_word(word):
 
 
 def get_highest_word_score(word_list):
-    #Me dan una lista con distintas palabras
-    #2.- Calcular el score de cada palabra 
-        #Iterar cada palabra en la word_list
-        #Con la funcion "score_word" saber el score de cada palabra
-        #Guardar el score de cada palabra en una nueva lista llamada "score_list"
-        #Hacer un diccionario que tenga como llaves el "word_list" y como valores "score_list"
-        #Obtener el score mas alto en los valores del diccionario (score_list)
-
-        #Si no hay duplicados en valores (scores), crear una tupla con el valor mayor { word , score }
-
-        #Si hay duplicados en valores (scores)
-            #Si el len(word) < 10 entonces el ganador es el del len menor
-            #Si el len(word) = 10 entonces el ganador es el de len 10
-            #Si hay mas de 1 palabra con len = 10, el ganador es el primero en la lista
-            #Si un word tiene el mismo score y mismo len, retornar el primero que esta en la lista
-
-
-
-
-
-
+        
+    score_list = []
+    dict_word_score = {}
+        
     
-    #5.- Si hay empate, buscar la palabra con menos letras, es decir el menor lenght --> solo si:
-        #no hay una palabra de 10 letras
-    #6.- Si hay una palabra con el mismo score y mismo largo, retornar el primero que este en la lista
+    for word in word_list:
+        score_for_each_word = score_word(word)
+        score_list.append(score_for_each_word)
+        score_list  
+        
+    dict_word_score = dict(zip(word_list,score_list))
+        
+    max_value = max(dict_word_score.values())
+    #crear diccionario con los top scores
+    #dict_top_score = {}
 
 
 
 
+
+word_list = ["cat","beach","pig"]
 
 print(get_highest_word_score(word_list))
