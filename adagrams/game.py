5d0dbf94bc03874c182044a459c2c4459d31220b
Hannah <hannah@chandleys.com>
2023-03-21 13:32:54 -0700

    Wrote score_word and established a dict with the letters and their point values. This function passed all tests in wave 3.

--- adagrams/game.py
+++ adagrams/game.py
@@ -30,6 +30,35 @@ LETTER_POOL = {
     'Z': 1
 }
 
+LETTER_VALUE = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+}
+
 def draw_letters():
     alphabet = list(LETTER_POOL.keys())
     drawn_letters = []
@@ -61,12 +90,21 @@ def uses_available_letters(word, letter_bank):
         if letter in available_letters:
             available_letters.remove(letter)
         else:
-            return False  
+            return False
     return True
 
 
 def score_word(word):
-    pass
+    word = word.upper()
+    score = 0
+
+    for letter in word:
+        score += LETTER_VALUE[letter]
+    
+    if len(word) > 6 and len(word) < 11:
+        score += 8
+    
+    return score
 
 
 def get_highest_word_score(word_list):
