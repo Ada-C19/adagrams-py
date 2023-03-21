a9e3ac6130c3c764bc47f99828d1a98b6dbcfd64
Divya <divya.naidoo5@gmail.com>
2023-03-20 21:49:14 -0700

    updated wave 1

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,17 @@
+import random
+
 def draw_letters():
-    pass
+    LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    random.choices(LETTERS, weights=(9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1), k=10)
+    
+    letter_bank = []
+    letter = random.choices(LETTERS, weights=(9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1), k=10)
+    letter_bank.append(letter)
+
+    print(letter_bank)
+
+draw_letters()
 
 def uses_available_letters(word, letter_bank):
     pass
