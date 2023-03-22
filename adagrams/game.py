2b9081e15953d7eebf39ff50efa61c7239043cfe
Diana <diana38974@gmail.com>
2023-03-22 19:05:59 -0400

    Function implementation to return score.

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,24 +1,7 @@
-
 def draw_letters():
     import random
-
     RANGE_LOW = 0
     RANGE_HIGH = 25
-
-#     LETTER_POOL = {
-#     'A': 1, 
-#     'B': 1, 
-#     'C': 1, 
-#     'D': 1, 
-#     'E': 1, 
-#     'F': 1, 
-#     'G': 1, 
-#     'H': 1, 
-#     'I': 1, 
-#     'J': 1, 
-#     'K': 1, 
-#     'L': 1 
-# }
 
     LETTER_POOL = {
     'A': 9, 
@@ -66,9 +49,9 @@ def draw_letters():
     print(hand)
     return hand                  
 
+
 def uses_available_letters(word, letter_bank):
     word = word.upper()
-
     is_word_valid = True
     for letter in word:
         if letter not in letter_bank:
@@ -83,7 +66,46 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    score_chart = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 6, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+    }
+    
+    word = word.upper()
+    score_sum = 0
+    for letter in word:
+        value = score_chart.get(letter)
+        score_sum += value
+    print(len(word))
+    if len(word) >= 7 and len(word) <= 10:
+        score_sum += 8
+    print(score_sum)
+    return(int(score_sum))
+
 
 def get_highest_word_score(word_list):
     pass
