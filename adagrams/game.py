ce119dd01577edc8d49d82d296bcbf24bdf03342
Susi Franco <susi.franco32@gmail.com>
2023-03-21 20:37:13 -0700

    refractored random.sample so that it doesn't have redundancy

--- adagrams/game.py
+++ adagrams/game.py
@@ -31,6 +31,7 @@ def draw_letters():
 }
     #make an empty list that will hold all available letters
     available_letters = []
+
     #looping through letter pool dic keys, and adding the key the amount
     #of times the value designates to available letters list
     for letter in LETTER_POOL.keys():
@@ -38,18 +39,18 @@ def draw_letters():
             available_letters.append(letter)
 
     #choose 10 random letters from available letters
-    chosen_letters = random.sample(available_letters,k=10)  
+    chosen_letters = random.sample(available_letters, 10)  
     return chosen_letters
 
 def uses_available_letters(word, letter_bank):
     
     letter_bank_copy = letter_bank.copy()
     #add a loop that checks if letter in word is in letter bank copy. 
+
     for letter in word.upper():
-        #if letter in bank copy remove the letter
+        #if letter in bank copy remove the letter, else return false and break
         if letter in letter_bank_copy:
             letter_bank_copy.remove(letter)
-        #otherwise break out of function and return false
         else:
             return False
     return True
@@ -88,6 +89,7 @@ def score_word(word):
     #sum up each letter in word to score, use .upper to make all caps
     for letter in word.upper():
         score+= score_values[letter]
+
     #add 8 points if len of word is >= 7
     if len(word) >= 7:
         score+= 8
@@ -98,6 +100,7 @@ def get_highest_word_score(word_list):
     #initialize word_score_dict and highest_score_list
     word_scores_dict = {}
     highest_score_list = []
+
     #add scores as keys to word_score_dict, and words as values in a list
     for word in word_list:
         score = score_word(word)
@@ -105,17 +108,19 @@ def get_highest_word_score(word_list):
             word_scores_dict[score].append(word)       
         else:
             word_scores_dict[score] = [word]
+
     #set high score by using max function on word_score_dic keys
     highest_score = max(word_scores_dict.keys())
+
     #set shortest word with min func using lambda to find shortest len word
-    shortest_word = min(word_scores_dict[highest_score], key = lambda word: len(word))
+    best_word = min(word_scores_dict[highest_score], key = lambda word: len(word))
+
     #loop through tie of high score words and make length ==10 best word
     for word in word_scores_dict[highest_score]:
         if len(word) == 10:
             best_word = word
             break
-        else:
-            best_word = shortest_word
+
     #add the best word and highest score to the highest_score_list   
     highest_score_list.append(best_word)
     highest_score_list.append(highest_score)
