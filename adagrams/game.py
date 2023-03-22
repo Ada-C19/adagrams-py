1316ae64e7ae394bee398ef73ef7c60d9389e1a2
Gabby <gabrielleyoung10@gmail.com>
2023-03-22 18:29:49 -0500

    Refactor loops and conditionals for better readability

--- adagrams/game.py
+++ adagrams/game.py
@@ -47,22 +47,19 @@ def get_highest_word_score(word_list):
     for word in word_list:
         score = score_word(word) 
         word_dict[word] = score
-    
-    for word in word_dict:
-        if word_dict[word] > max_score:
-            max_score = word_dict[word]
-            winning_word = word
-            
-    matching_scores.append(winning_word)
 
-    # Look for other keys with the same score as max score 
+    max_score = max(word_dict.values())
+
+    # Look for keys with the same score as max_score 
     # Append them to the matching_scores list
-    for word_key in word_dict:
-        if word_key != winning_word and word_dict[word_key] == max_score:
-            matching_scores.append(word_key)
+    for word in word_dict:
+        if word_dict[word] == max_score:
+            matching_scores.append(word)
 
     for word in matching_scores:
-        if len(word) == 10:
+        if len(matching_scores) == 1:
+            winning_word = word
+        elif len(word) == 10:
             winning_word = word
             break 
         else:   
