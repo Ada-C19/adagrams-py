41197e92842cc4bdbe58ccb1ac7a5559c1618f82
Diana <diana38974@gmail.com>
2023-03-21 23:40:19 -0400

    Wave 01 has been completed

--- adagrams/game.py
+++ adagrams/game.py
@@ -6,46 +6,65 @@ def draw_letters():
     RANGE_HIGH = 25
 
 #     LETTER_POOL = {
-#     'A': 9, 
-#     'B': 2, 
-#     'C': 2, 
-#     'D': 4, 
-#     'E': 12, 
-#     'F': 2, 
-#     'G': 3, 
-#     'H': 2, 
-#     'I': 9, 
+#     'A': 1, 
+#     'B': 1, 
+#     'C': 1, 
+#     'D': 1, 
+#     'E': 1, 
+#     'F': 1, 
+#     'G': 1, 
+#     'H': 1, 
+#     'I': 1, 
 #     'J': 1, 
 #     'K': 1, 
-#     'L': 4, 
-#     'M': 2, 
-#     'N': 6, 
-#     'O': 8, 
-#     'P': 2, 
-#     'Q': 1, 
-#     'R': 6, 
-#     'S': 4, 
-#     'T': 6, 
-#     'U': 4, 
-#     'V': 2, 
-#     'W': 2, 
-#     'X': 1, 
-#     'Y': 2, 
-#     'Z': 1
+#     'L': 1 
 # }
 
-    list_of_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "k", \
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+    list_of_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', "I", "J", "K", \
                        "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     hand = []
     while len(hand) < 10:
         random_number = random.randint(RANGE_LOW, RANGE_HIGH)
         letter = list_of_letters[random_number]
-        hand.append(letter)
-    print(hand)
-    return hand
+        letter_value = LETTER_POOL.get(letter)
 
-
-
+        if letter in hand:
+            n = hand.count(letter)
+            if n < letter_value:
+                hand.append(letter)
+        else:
+            hand.append(letter)
+    # print(hand)
+    return hand                  
 
 def uses_available_letters(word, letter_bank):
     pass
