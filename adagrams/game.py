b828a6290e847a04b836ad33336c738d708eba14
Aiyana Ball <ballaiyana@gmail.com>
2023-03-21 18:18:24 -0700

    wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -18,6 +18,21 @@ DISTRIBUTION_OF_LETTERS = {
     "Y": 2, "Z": 1
 }
 
+SCORE_CHART = {
+    "A": 1, "B": 3,
+    "C": 3, "D": 2,
+    "E": 1, "F": 4,
+    "G": 2, "H": 4,
+    "I": 1, "J": 8,
+    "K": 5, "L": 1,
+    "M": 3, "N": 1,
+    "O": 1, "P": 3,
+    "Q": 10, "R": 1,
+    "S": 1, "T": 1,
+    "U": 1, "V": 4,
+    "W": 4, "X": 8,
+    "Y": 4, "Z": 10
+}
 
 def draw_letters():
     letter_bank = []
@@ -45,17 +60,39 @@ def uses_available_letters(word, letter_bank):
         if frequency_in_bank < frequency_in_word:
             return False
     return True
-            
-
 
 
 def score_word(word):
-    pass
+    score_total = 0
+    for letter in word:
+        letter = letter.upper()
+        letter_points = SCORE_CHART[letter]
+        score_total += letter_points
+    if len(word) >= 7:
+        score_total += 8
+    # print(score_total)
+    return score_total
 
 
 def get_highest_word_score(word_list):
-    pass
+    words_and_scores = {}
+    for i in word_list:
+        words_and_scores[i] = score_word(i)
+    print(words_and_scores)
+    # word_scores = []
+    # # print(word_scores)
+    # for word in word_list:
+    #     word_score = score_word(word)
+    #     word_scores.append(word_score)
+        # words_and_scores[i] = word_score
+    # # print(word_scores)
+    # for i in word_scores:
+    #     words_and_scores[i] = i
+    
 
 
+word_list = ["dog", "banana", "dewey"]
 # draw_letters()
 # uses_available_letters()
+# score_word()
+get_highest_word_score(word_list)
