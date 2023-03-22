5e8d32f1e22946a520de7725fb488dc0ace370cf
Lindsay <lindsaybolzwilson@gmail.com>
2023-03-22 18:42:16 -0400

    Added comments and fixed wave 1, made sure it didnt remove letters from pool as it iterates.

--- adagrams/game.py
+++ adagrams/game.py
@@ -16,7 +16,7 @@ LETTER_VALUES = {
     'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 
     'Z': 10
 }
-
+VOWELS = ["A", "E", "I", "O", "U", "Y"]
 
 def draw_letters():
     # build the pool of letters as a list with all 
@@ -26,50 +26,80 @@ def draw_letters():
         for i in range(value):
             all_letters.append(key)
 
-    # build the hand
-    hand = []
-    for i in range(10):
-        random_letter = choice(all_letters)
-        # add the letter to the hand
-        hand.append(random_letter)
-        # make sure if it was the 1 z it cant be selected again
-        all_letters.remove(random_letter)
-
+    # build the hand making sure at least 3 letters are vowels
+    vowel_count = 0
+    while vowel_count < 3:
+        hand = []
+        for i in range(10):
+            random_letter = choice(all_letters)
+        
+            # make sure we dont exceed the number allowed for that letter
+            random_letter_count_in_hand = hand.count(random_letter)
+            while random_letter_count_in_hand == LETTER_POOL[random_letter]:
+                random_letter = choice(all_letters)
+                random_letter_count_in_hand = hand.count(random_letter)
+            
+            # add the letter to the hand
+            hand.append(random_letter)
+        
+        # makes sure vowel_count is updated before we go through the loop again.
+        vowel_count = 0
+        for vowel in VOWELS:
+            vowel_count += hand.count(vowel)
+        print(f"{vowel_count=}")
     return hand
 
+
 def uses_available_letters(word, letter_bank):
-    print(f"{word=}")
-    print(f"{letter_bank=}")
+    # formats the word list
     word_list = list(word.upper())
     for letter in word_list:
+        # checks if letter is in the letter bank.
         if letter not in letter_bank:
             return False
+        
+        # make sure there are not too many of a letter
         if word_list.count(letter) > letter_bank.count(letter):
             return False
     return True
 
+
 def score_word(word):
     score = 0
+
+    # Iterate through the word and add to the score as we go
     for letter in list(word.upper()):
         score += LETTER_VALUES[letter]
+    
+    # if the len of the word is > 7 add the bonus
     if len(word) >= 7:
         score += 8
     return score
 
+
 def get_highest_word_score(word_list):
+    # initialize the best score and the list with a word and score tuple
     words_and_scores = []
     best_score = tuple([0, 0])
     for word in word_list:
         words_and_scores.append(tuple([word, score_word(word)]))
+
+    # Iterate through the words and scores list and check their values
     for tup in words_and_scores:
+        # update best score if a score is higher than the current best score value
         if tup[1] > best_score[1]:
             best_score = tup
         # deal with a tie
         if tup[1] == best_score[1]:
-            # sets it to new tup if its shorter 
+            # sets best score to new tup if its len is 10 then returns becasue the first
+            # one of these answers will always win so we will just return if we get here 
             if len(tup[0]) == 10:
                 best_score = tup
                 return best_score
+            # sets best score to new tup if its shorter 
+            # if there are multiple of this situation, 
+            # it will continue to find the shortest
+            # otherwise, it wont change, making the existing best score stand
             elif len(tup[0]) < len(best_score[0]):
                 best_score = tup
 
