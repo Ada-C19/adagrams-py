600d7e43c593e32a4595f891cfa1bf5c9240b013
Lyuda Kim <lyudarkim@gmail.com>
2023-03-21 18:38:33 -0700

    Refactored wave 3 score word function by using a dictionary.

--- adagrams/game.py
+++ adagrams/game.py
@@ -34,10 +34,12 @@ def draw_letters():
     # Store the quantity of each letter in the pool and the count of each letter in these 2 lists
     counts = []
     letters = []
+
     # Get the letters and how many of each letter there are in the pool
     for letter, count in LETTER_POOL.items():
         letters.append(letter)
         counts.append(count)
+        
     # Draw a hand of 10 letters from the letter pool
     hand = random.sample(letters, counts=counts, k=10)
 
@@ -68,13 +70,16 @@ def uses_available_letters(word, letter_bank):
     # Convert case on the word string and the letter bank list
     word = convert_case(word)
     letter_bank = convert_case(letter_bank)
+    
     # Iterate through the letters in the word to check if they're in the letter bank
     for letter in word:
         if letter not in letter_bank:
             return False
+    
     # Check how many times a letter occurs in the word and the letter bank
     frequency_word = count_letter_frequency(word)
     frequency_bank = count_letter_frequency(letter_bank)
+    
     # Compare the word frequency in the word and the letter bank
     for letter in word:
         if not (frequency_word[letter] <= frequency_bank[letter]):
@@ -85,43 +90,44 @@ def uses_available_letters(word, letter_bank):
 
 def score_word(word):
     points_total = 0
-    # score_chart = {
-    #     ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"): 1,
-    #     ("D", "G"): 2,
-    #     ("B", "C", "M", "P"): 3,
-    #     ("F", "H", "V", "W", "Y"): 4,
-    #     ("K"): 5,
-    #     ("J", "X"): 8,
-    #     ("Q", "Z"): 10,
-    # }
-    # # Get the value of "A"
-    
     word = convert_case(word)
 
+    score_chart = {
+        "A": 1,
+        "E": 1, 
+        "I": 1,
+        "O": 1,
+        "U": 1,
+        "L": 1,
+        "N": 1,
+        "R": 1,
+        "S": 1,
+        "T": 1,
+        "D": 2,
+        "G": 2,
+        "B": 3,
+        "C": 3,
+        "M": 3,
+        "P": 3,
+        "F": 4,
+        "H": 4,
+        "V": 4,
+        "W": 4,
+        "Y": 4,
+        "K": 5,
+        "J": 8,
+        "X": 8,
+        "Q": 10,
+        "Z": 10,
+    }
+    
+    # If the string is empty
+    if not word:
+        return 0
+    
+    # Add up the points by looking up each letter in word in the score_chart dict
     for letter in word:
-        if not word:
-            return 0
-        
-        if letter == "A" or letter == "E" or letter == "I" or letter == "O" or letter == "U" or letter == "L" or letter == "N" or letter == "R" or letter == "S" or letter == "T":
-            points_total += 1
-
-        if letter == "D" or letter == "G":
-            points_total += 2
-
-        if letter == "B" or letter == "C" or letter == "M" or letter == "P":
-            points_total += 3
-
-        if letter == "F" or letter == "H" or letter == "V" or letter == "W" or letter == "Y":
-            points_total += 4
-
-        if letter == "K":
-            points_total += 5
-
-        if letter == "J" or letter == "X":
-            points_total += 8
-
-        if letter == "Q" or letter == "Z":
-            points_total += 10
+        points_total += score_chart[letter]
 
     if len(word) > 6:
         points_total += 8
@@ -166,3 +172,4 @@ def get_highest_word_score(word_list):
         
         if score == highest_score and length == shortest_highest:
             return word, score
+
