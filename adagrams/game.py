037a967204ce048fd9a525bd7ec75fdf1dea9fe7
Jamie <jamie.danielle@denverrollerderby.org>
2023-03-22 16:54:54 -0600

    wave 03

--- adagrams/game.py
+++ adagrams/game.py
@@ -32,7 +32,34 @@ LETTER_POOL = {
     'Z': 1
 }
 
-
+score_chart = {
+    'A': 1,
+    'B': 3,
+    'C': 3,
+    'D': 2,
+    'E': 1,
+    'F': 4,
+    'G': 2,
+    'H': 4,
+    'I': 1,
+    'J': 8,
+    'K': 5,
+    'L': 1,
+    'M': 3,
+    'N': 1,
+    'O': 1,
+    'P': 3,
+    'Q': 10,
+    'R': 1,
+    'S': 1,
+    'T': 1,
+    'U': 1,
+    'V': 4,
+    'W': 4,
+    'X': 8,
+    'Y': 4,
+    'Z': 10
+  }
 
 def draw_letters():
     players_hand = []
@@ -61,7 +88,10 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    total_score = sum(score_chart.get(letter.upper(), 0) for letter in word)
+    if len(word) in [7, 8, 9, 10]:
+        total_score += 8
+    return total_score
     
 
 def get_highest_word_score(word_list):
