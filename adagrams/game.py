70556ceb299e4cac46be6caa7924e65922d1be1b
Zhuo <zhuo.parse@gmail.com>
2023-03-21 22:06:05 -0400

    modified wave 2, 3, 4

--- adagrams/game.py
+++ adagrams/game.py
@@ -29,23 +29,73 @@ LETTER_POOL = {
 }
 def draw_letters():
     draw_letters_list = []
-    letters_freq_dic = {}
+    letter_freq = {}
     while len(draw_letters_list) < 10:
         cur_random_letter = random.choice(list(LETTER_POOL.keys()))
-        if cur_random_letter in letters_freq_dic.keys():
-            letters_freq_dic[cur_random_letter] += 1
+        if cur_random_letter in letter_freq.keys():
+            letter_freq[cur_random_letter] += 1
         else:
-            letters_freq_dic[cur_random_letter] = 1
-        if letters_freq_dic[cur_random_letter] > LETTER_POOL[cur_random_letter]:
+            letter_freq[cur_random_letter] = 1
+        if letter_freq[cur_random_letter] > LETTER_POOL[cur_random_letter]:
             continue
         else:
             draw_letters_list. append(cur_random_letter)
     return draw_letters_list
 def uses_available_letters(word, letter_bank):
-    pass
+    letter_bank_freq = {}
+    for letter in letter_bank:
+        if letter in letter_bank_freq:
+            letter_bank_freq[letter] += 1
+        else:
+            letter_bank_freq[letter] = 1
+    for letter in word:
+        if letter and letter.upper() not in letter_bank:
+            return False
+        else:
+            if letter in letter_bank:
+                letter_bank_freq[letter] -= 1
+                if letter_bank_freq[letter] < 0:
+                    return False
+            else:
+                letter_bank_freq[letter.upper()] -= 1
+                if letter_bank_freq[letter.upper()] < 0:
+                    return False
+    return True
+    
 
 def score_word(word):
-    pass
+    score_dic = {}
+    score_dic[1] = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T" ]
+    score_dic[2] = ["D", "G" ]
+    score_dic[3] = ["B", "C", "M", "P"]
+    score_dic[4] = ["F", "H", "V", "W", "Y" ]
+    score_dic[5] = ["K"]
+    score_dic[8] = ["J","X"]
+    score_dic[10] = ["Q","Z"]
+
+    score_sum = 0
+    for letter in word:
+        for key, value in score_dic.items():
+            if letter in value or letter.upper() in value:
+                score_sum += key
+    if 7 <= len(word) and len(word) <= 10:
+        score_sum += 8
+    return score_sum
 
 def get_highest_word_score(word_list):
-    pass
+    highest_score = 0
+    highest_letter = ""
+    for word in word_list:
+        cur_score = score_word(word)
+        if cur_score > highest_score:
+            highest_score = cur_score
+            highest_letter = word
+        elif cur_score == highest_score:
+            if len(highest_letter) == 10 and len(word) == 10:
+                continue
+            elif len(word) == 10:
+                highest_letter = word
+            elif len(highest_letter) != 10 and len(word) < len(highest_letter):
+                highest_letter = word
+
+    return highest_letter, highest_score
