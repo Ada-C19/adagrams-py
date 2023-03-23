2955efd7cef7e6e93c8c5750720818d6c75268e8
Ariel Williams <arielwilliams121013@gmail.com>
2023-03-22 18:58:22 -0700

    Created alphabet_letters.py which holds letter bank dict. Updated game.py to import alphabet_letters.py. Confirmed generate_alphabet_letters() created letter bank dictionary.

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,10 @@
+# import everything (alphabet_dictionary) from alphabet_letters file
+from adagrams.alphabet_letters import *
+
+# wave 1
 def draw_letters():
-    pass
+    alphabet_dictionary = generate_alphabet_letters() 
+
 
 def uses_available_letters(word, letter_bank):
     pass
