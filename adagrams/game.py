513b40d04b508f95ef87f683d317e117401c0817
Say Ryder <lindz.ryder@gmail.com>
2023-03-23 18:58:10 -0400

    wave 3 good to go

--- adagrams/game.py
+++ adagrams/game.py
@@ -65,9 +65,8 @@ def uses_available_letters(word, letter_bank):
   
     if len(word) <= len(letter_bank):
         for letter in letter_bank:
-            if word.count(letter) <= letter_bank.count(letter):
-                continue
-            elif word.count(letter) > letter_bank.count(letter):
+            # erased if statement for continue
+            if word.count(letter) > letter_bank.count(letter):
                 return False
 
         for letter in word:
@@ -91,9 +90,46 @@ def uses_available_letters(word, letter_bank):
     else:
         return False
     
-
 def score_word(word):
-    pass
+    score_value_dict = {
+                    'A': 1, 
+                    'B': 3, 
+                    'C': 3, 
+                    'D': 2, 
+                    'E': 1, 
+                    'F': 4, 
+                    'G': 2, 
+                    'H': 4, 
+                    'I': 1, 
+                    'J': 8, 
+                    'K': 5, 
+                    'L': 1, 
+                    'M': 3, 
+                    'N': 1, 
+                    'O': 1, 
+                    'P': 3, 
+                    'Q': 10, 
+                    'R': 1, 
+                    'S': 1, 
+                    'T': 1, 
+                    'U': 1, 
+                    'V': 4, 
+                    'W': 4, 
+                    'X': 8, 
+                    'Y': 4, 
+                    'Z': 10
+}
+    word = list(word.upper())
+    score = 0
+
+    if len(word) >= 7:
+        score += 8
+
+    for letter in word:
+        if letter in score_value_dict:
+            score += score_value_dict[letter]
+    
+    return score
 
 def get_highest_word_score(word_list):
     pass
