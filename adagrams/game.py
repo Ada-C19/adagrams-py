9e72501edee57606079b838ee3f089be166c4554
Danica <danicas.edu@gmail.com>
2023-03-24 00:01:51 +0800

    pass wave 2

--- adagrams/game.py
+++ adagrams/game.py
@@ -28,61 +28,35 @@ LETTER_POOL = {
     'Z': 1
 }
 
-
-
-# How do I get a big list with A * 9 etc?
-# Why do I need to make a copy of my dictionary?
-# letter_pool_list = list(LETTER_POOL)
-
-# ********* SOLUTION 1 ******************
-
 def draw_letters():
     big_list = []
-
     for letter, letter_quantity in LETTER_POOL.items():
-        # loop needs to restart here
         big_dict = letter * letter_quantity
         for single_char in big_dict:
             big_list.append(single_char)
-        # return big_list
-    # print(big_list)
 
-# we want to draw 10 random letters from the big_list
     draw_of_10_letters = []
     while len(draw_of_10_letters) < 10:
         random_letter = random.choice(big_list)
         draw_of_10_letters.append(random_letter)
         big_list.remove(random_letter)
-    print(draw_of_10_letters)
-
-    # for item in big_list:    
-    #     random_letter = random.choice(big_list)
-    #     draw_of_10_letters.append(random_letter)
-    #     if len(draw_of_10_letters) == 9:
-    #         print(draw_of_10_letters)
     return draw_of_10_letters
 
 draw_letters()   
 
-# we need to pop() a letter each iteration/draw
-# ************ SOLUTION 1 END ******************
-
-
-
-# def letter_frequency():
-# We should only have the values as the quantity of the letters
-# How do we establish
-# What does distribution mean?
-# keys * values?
-
-    # The value of the keys of LETTER_POOL is its quantity
-    # 
-
-
-# ******* TEST 2 **********
 def uses_available_letters(word, letter_bank):
-    pass
 
+        word = word.upper()
+        for letter in word:
+            word_count = word.count(letter)
+            letter_bank_count = letter_bank.count(letter)
+            if letter not in letter_bank:
+                return False
+            if word_count > letter_bank_count:
+                return False
+        return True
+
+# ******* TEST 3 **********
 def score_word(word):
     pass
 
