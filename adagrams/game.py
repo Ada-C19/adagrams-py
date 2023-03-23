28cb37ddb34a5146b225be57ad3b4fc5b294f563
Ericka <erickamoreno529@gmail.com>
2023-03-23 14:48:46 -0700

    passed wave_2 functions

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,16 +1,21 @@
 import random 
+import copy
 
-# pool_of_letters_amount = [
-#     {"A": 9}, {"B": 2}, {"C": 2}, {"D": 4}, {"E": 1}, {"F": 2}, {"G": 3}, {"H": 2}, {"I": 9}, {"J": 1}, {"K": 1}, {"L": 4}, {"M": 2}, 
-#     {"N": 6}, {"O": 8}, {"P": 2}, {"Q": 1}, {"R": 6}, {"S": 4}, {"T": 6}, {"U": 4}, {"V": 2}, {"W": 2}, {"X": 1}, {"Y": 2}, {"Z": 1}, 
-# ]
+letter_pool_frequency = {
+    "A": 9, "B": 2, "C": 2, "D": 4, "E": 12, "F": 2, "G": 3, "H": 2, "I": 9, "J": 1, "K": 1, "L": 4, "M": 2, 
+    "N": 6, "O": 8, "P": 2, "Q": 1, "R": 6, "S": 4, "T": 6, "U": 4, "V": 2, "W": 2, "X": 1, "Y": 2, "Z": 1, 
+}
 
-# pool_of_letters_score = [
-#     {"A": 1},{"B": 3},{"C": 3},{"D": 2},{"E": 1},{"F": 4},{"G": 2},{"H": 4}, {"I": 1},{"J": 8},{"K": 5},{"L": 1},{"M": 3},
-#     {"N": 1},{"O": 1},{"P": 3},{"Q": 1},{"R": 1},{"S": 1},{"T": 1},{"U": 1},{"V": 4},{"W": 4}, {"X": 8},{"Y": 4},{"Z": 1},
-# ]
+letter_pool_score = {
+    "A": 1,"B": 3,"C": 3,"D": 2,"E": 1,"F": 4,"G": 2,"H": 4,"I": 1,"J": 8,"K": 5,"L": 1,"M": 3,
+    "N": 1,"O": 1,"P": 3,"Q": 1,"R": 1,"S": 1,"T": 1,"U": 1,"V": 4,"W": 4,"X": 8,"Y": 4,"Z": 1,
+}
 
-pool_of_letters = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", 
+# print (letter_pool_frequency["A"]) --> 9
+# print (letter_pool_score["A"]) --> 1
+
+
+letter_pool = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", 
                 "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", 
                 "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", 
                 "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", 
@@ -18,52 +23,38 @@ pool_of_letters = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "
 
 #####WAVE_1_FUNCTION#####
 def draw_letters():
-    
-    letters_drawn = 0
+    copied_letter_pool = list(letter_pool)
     hand_of_letters = []
     while len(hand_of_letters) < 10:
-        copied_pool_of_letters = pool_of_letters.copy()
-        new_letter = random.choice(copied_pool_of_letters)
+        new_letter = random.choice(copied_letter_pool)
         hand_of_letters.append(new_letter)
-        copied_pool_of_letters.remove(new_letter)
-        letters_drawn += 1
+        copied_letter_pool.remove(new_letter)
     return hand_of_letters
 
-
-
-
 draw_letters()
-#####DON'T_TOUCH!!!#####
-
-#testing if these are the same files 
+#####END OF WAVE 1 FUNCTION#####
 
 
-letter_bank = draw_letters()
-# print (letter_bank)
-# word = input("What word do you pick?")
 
+#####WAVE_2_FUNCTION#####
 def uses_available_letters(word, letter_bank):
-    print (letter_bank)
+    letters_copy = letter_bank[:]
+    word = word.upper()
     for letter in word:
         print (letter)
-        if letter not in letter_bank:
-            print ("False")
+        if letter not in letters_copy:
             return False
         else: 
-            letter_bank.remove(letter)
-    #         print (letter_bank)
-    # print ("True")
-    # return True
-        
-
-uses_available_letters("AAA", ["A", "X", "X", "X", "X", "X", "X", "X", "X", "X"])
+            letters_copy.remove(letter)
+    return True
+#####END_OF_WAVE_2_FUNCTION#####
 
 
 
-
-
+#####WAVE_3_FUNCTION#####
 def score_word(word):
     pass
+#####END_OF_WAVE_3_FUNCTION#####
 
 def get_highest_word_score(word_list):
     pass
