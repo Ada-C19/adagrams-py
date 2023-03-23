8f2f693510f87dace234b08da9bcbcd5ab4c700c
Linh Huynh <greenlinh@gmail.com>
2023-03-23 01:17:35 -0700

    Made word scoring process case insensitive.

--- adagrams/game.py
+++ adagrams/game.py
@@ -58,6 +58,35 @@ SCORE_CHART = {
   'Z': 10
 }
 
+SCORE_CHART_LOWERCASE = {
+  'a': 1,
+  'b': 3,
+  'c': 3,
+  'd': 2,
+  'e': 1,
+  'f': 4,
+  'g': 2,
+  'h': 4,
+  'i': 1,
+  'j': 8,
+  'k': 5,
+  'l': 1,
+  'm': 3,
+  'n': 1,
+  'o': 1,
+  'p': 3,
+  'q': 10,
+  'r': 1,
+  's': 1,
+  't': 1,
+  'u': 1,
+  'v': 4,
+  'w': 4,
+  'x': 8,
+  'y': 4,
+  'z': 10
+}
+
 temp_list = []
 LETTER_POOL_LIST = []
 hand = []
@@ -119,9 +148,9 @@ def score_word(word):
     word_score = {}
     for letter in word:
        if letter in SCORE_CHART:
-          #letter_point = SCORE_CHART[letter]
-          #word_score.append(letter_point)
           word_score[letter] = SCORE_CHART[letter]
+       elif letter in SCORE_CHART_LOWERCASE:
+          word_score[letter] = SCORE_CHART_LOWERCASE[letter]
     #print(word_score)
 
     return sum(word_score.values())
