e077ccd637d3c4cc2e9b6ff99ce209801376ef1c
Aisha Mohamed <amdahir1@gmail.com>
2023-03-23 17:28:49 -0500

    All tests passed, cleaned up

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,45 +1,40 @@
 import random
 import array
 
+# manually typed list of all the letters (and how many of them as repeats) that are allowed
 LETTER_LIST = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F', 'G', 'G', 'G', 'H', 'H', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'J', 'K', 'L', 'L', 'L', 'L', 'M', 'M', 'N', 'N', 'N', 'N', 'N', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'P', 'P', 'Q', 'R', 'R', 'R', 'R', 'R', 'R', 'S', 'S', 'S', 'S', 'T', 'T', 'T', 'T', 'T', 'T', 'U', 'U', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'Y', 'Y', 'Z']
 
 def draw_letters():
-    letters = []
+    letters = [] # empty 'letters' list 
 
-    letters = random.sample(LETTER_LIST,10)
-
-    # random.shuffle(LETTER_LIST)
-    # letters = LETTER_LIST[:10]
-
-    # while len(letters) < 10:
-    #     letter = random.choice(LETTER_LIST)
-    #     letters.append(letter)
-        # LETTER_LIST.remove(letter)
+    letters = random.sample(LETTER_LIST,10) #10 letters are randomly selected and set to the letters list
     
-    return letters
+    return letters #returns 10 randomly selected letters 
 
 def uses_available_letters(word, letter_bank):
-    # for i in word:
-    #     if i not in letter_bank:
-    #         return False
-    #     else:
-    #         # letter_bank.remove(i)
-    #         return True
-    word_ =  []
-    letter_bank_ = []
 
+    #loops through all the letters in the words and makes them lowercase strings in a new list
+    word_case_insensitive =  []
     for i in word:
-        word_.append(i.casefold())
-    for i in letter_bank:
-        letter_bank_.append(i.casefold())
+        word_case_insensitive.append(i.lower())
 
-    counter = 0
-    for i in word_:
-        for j in letter_bank_:
+    #loops through all the letters in the letter bank and makes them lowercase strings in a new list
+    letter_bank_case_insensitive = []
+    for i in letter_bank:
+        letter_bank_case_insensitive.append(i.lower())
+
+    #counter for how many loops are completed
+    loop_count = 0
+
+    #for-loop that compares each item in both lists, and removes from letter bank if there's a match
+    for i in word_case_insensitive:
+        for j in letter_bank_case_insensitive:
             if i == j:
-                counter += 1
-                letter_bank_.remove(j)
-    if counter == len(word):
+                loop_count += 1
+                letter_bank_case_insensitive.remove(j)
+
+    #if the loop count and length of a word are equal, it returns True and else False
+    if loop_count == len(word):
         return True
     else:
         return False
@@ -48,14 +43,7 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    score = 0
-    # score_1 = ['A', 'E', 'I', 'O', 'U', 'L' ,'N','R','S','T']
-    # score_2 = ['D', 'G']
-    # score_3 = ['B', 'C', 'M', 'P']
-    # score_4 = ['F', 'H', 'V', 'W','Y']
-    # score_5 = ['K']
-    # score_8 = ['J','X']
-    # score_10 = ['Q','Z']
+    #lists of scores grouped by number of points. Upper and lower are included to cover all bases (even though it might be not needed since the letters in the word are turned into lowercase)
     score_1 = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'L', 'l','N', 'n', 'R', 'r', 'S','s', 'T','t']
     score_2 = ['D', 'd', 'G', 'g']
     score_3 = ['B', 'b', 'C', 'c', 'M', 'm','P', 'p']
@@ -64,179 +52,80 @@ def score_word(word):
     score_8 = ['J','j','X', 'x']
     score_10 = ['Q', 'q','Z', 'z']
 
+    # Loops through all the letters in the word variable and makes them lowercase strings in a new list
     word_ =  []
-
     for i in word:
-        word_.append(i.casefold())
-
-    counter = 0
+        word_.append(i.lower())
+    
+    #variable is set and intalied to 0 to keep score
+    score = 0
+    #for each letter in 'word_' variable, if its in one of the lists it will add to the score accordingly 
     for i in word_: 
-        # if i == 'A' or 'E' or 'I' or 'O' or 'U' or 'L' or 'N' or 'R' or 'S' or 'T':
         if i in score_1:
             score += 1
-            # counter += 1
-        # elif i == 'D' or 'G': 
         elif i in score_2:
             score += 2
-            # counter += 1
-        # elif i == 'B' or 'C' or 'M' or 'P':
         elif i in score_3:
             score += 3  
-            # counter += 1
-        # elif i == 'F' or 'H' or 'V' or 'W' or 'Y':
         elif i in score_4:
             score += 4
-            # counter += 1
-        # elif i == 'K':
         elif i in score_5:
             score += 5
-            # counter += 1
-        # elif i == 'J' or 'X':
         elif i in score_8:
             score += 8
-            # counter += 1
-        # elif i == 'Q' or 'Z':
         elif i in score_10:
             score += 10
-            # counter += 1
     
+    #if the length of the word is in between (and including) 7 or 10, it will add 8 points to the score total
     if len(word_) <= 10 and len(word_) >= 7:
         score += 8
-    
 
-    # if len(word_) == 7 or 8 or 9 or 10: # still dont understand why this doesnt work, come back
-    #     score += 8
-    # else: 
-    #     score += 0
-
-    # if counter == 7 or 8 or 9 or 10:
-    #         score += 8
-    # else:
-    #         score += 0
-
-    # counter = 0
-    # while counter < 1:
-    #     if len(word) == 7 or 8 or 9 or 10:
-    #         score += 8
-    #     counter +=1
-
+    #returns final score
     return score
         
 
 def get_highest_word_score(word_list):
+    #word list is converted into tuple pairs. First is the word, second is the score called in from the score_word function for the word
     tuple_list = []
     for i in word_list:
         tuple_list.append((i, score_word(i)))
 
-    # for i in tuple_list:
+    #a new vairable is set to the sorted list of tuple_list, in decreasing order of the scores (highest to lowest)
+    highest_score_tuple_list = sorted(tuple_list, key=lambda t: t[1], reverse = True)
 
-    best_word_list = sorted(tuple_list, key=lambda t: t[1], reverse = True)
-    highest_score = best_word_list[0][1]
-
+    #a variable set to the first item's second element for highest score as just an integer 
+    highest_score = highest_score_tuple_list[0][1]
     
-    best_word_list_2 = []
-    for i in best_word_list:
+    #this helps group all the tuples that are tied by adding them to a new list if theyre equal to the highest score
+    tuples_list_tied_scores_only = []
+    for i in highest_score_tuple_list:
         if i[1] == highest_score:
-            best_word_list_2.append(i)
-            
-    best_word_list_3 = sorted(best_word_list_2, key=lambda x: len(x[0]))
-    best_word_list_4 = []
+            tuples_list_tied_scores_only.append(i)
 
+    #this sorts the tied tuples by length of the first item (the word) from shortest to longest             
+    tied_tuples_by_word_length = sorted(tuples_list_tied_scores_only, key=lambda x: len(x[0]))
+
+    #this creates a new list of added winning tuples by comparing it to the orginal order  
+    tuple_list_original_order = []
     for i in tuple_list:
-        if i in best_word_list_3:
-            best_word_list_4.append(i)
+        if i in tied_tuples_by_word_length:
+            tuple_list_original_order.append(i)
 
+    #
     best_word_list_over_ten = []
-
-    for i in best_word_list_4:
+    for i in tuple_list_original_order:
         if len(i[0]) == 10:
             best_word_list_over_ten.append(i)
 
-    if len(best_word_list_4) == 1:
-        best_word = best_word_list_4[0]
+    if len(tuple_list_original_order) == 1:
+        best_word = tuple_list_original_order[0]
     elif len(best_word_list_over_ten) > 0:
         best_word = best_word_list_over_ten[0]
     else:
-        best_word = best_word_list_3[0]
+        best_word = tied_tuples_by_word_length[0]
     
-
-
-    # for i in best_word_list_3:
-    #     if len(i[0]) == 10:
-    #         best_word = i
-    #     else:
-    #         best_word = best_word_list_3[0]
-    
-    # if len(best_word_list_4) == 1:
-    #     best_word = best_word_list_4[0]
-    # elif len(best_word_list_4) > 1:
-    #     if len(best_word_list_4[0][0]) == len(best_word_list_4[1][0]):
-    #         best_word = best_word_list_4[0]
-    #     # if best_word_list_4[0][1] == best_word_list_4[1][1]:
-    #     #     best_word = best_word_list_over_ten[0]
-    #     elif len(i[0]) == 10:
-    #         best_word = i
-    #     else:
-    #         best_word = best_word_list_3[0]
-
-    # for i in best_word_list_3:
-    #     if len(i[0]) == 10:
-    #         best_word = i
-    #     elif len(best_word_list_4[0][0]) == len(best_word_list_4[1][0]):
-    #         best_word = best_word_list_4[0]
-    #     else:
-    #         best_word = best_word_list_3[0]
-        
-    # for i in best_word_list_3:
-    #     if len(best_word_list_4) == 1:
-    #         best_word = best_word_list_4[0]
-    #     elif len(best_word_list_4) > 1:
-    #         if len(i[0]) == len(i[1]):
-    #             best_word = best_word_list_4[0]
-    #         elif len(i[0]) == 10:
-    #             best_word = i
-    #         else:
-    #             best_word = best_word_list_3[0]
-
-            
-
-    # tie_breaker_list = []
-
-    # for i in best_word_list:
-    #     if i[1] == best_word_list[0][1]:
-    #         tie_breaker_list.append(i)
-
-    # tie_breaker_list_sorted = sorted(tie_breaker_list, key=lambda x: len(x[0]))
-
-    # for i in tie_breaker_list_sorted:
-    #     if len(tie_breaker_list_sorted[0][0]) == len(tie_breaker_list_sorted[1][0]):
-    #         if tie_breaker_list_sorted[0][0] == word_list[0]:
-    #             best_word = tie_breaker_list_sorted[0]
-    #         else:
-    #             best_word = tie_breaker_list_sorted[1]
-
-                
-    #     elif len(i[0]) == 10:
-    #         best_word = i
-    #     else:
-    #         best_word = tie_breaker_list_sorted[0]
 
     return best_word
     
 
-    # if best_word_list[0] > best_word_list[1]:
-    #     best_word = best_word_list[0]
-    # elif best_word_list[0] == best_word_list[1] and best_word_list[1] > best_word_list[2]:
-    #     if len(best_word_list[0][0]) > len(best_word_list[1][0]):
-    #         best_word = best_word_list[1]
-    #     else:
-    #         best_word = best_word_list[0]
-    # elif best_word_list[0] == best_word_list[1] == best_word_list[2] and best_word_list[2] > best_word_list[3]:
-    #     if len(best_word_list[0][0]) > len(best_word_list[1][0]):
-    #         best_word = best_word_list[1]
-    #     elif len(best_word_list[2][0]) > len(best_word_list[1][0]
-    # elif best_word_list[0] == best_word_list[1] == best_word_list[2] == best_word_list[3] and best_word_list[3] > best_word_list[4]:
-
-
-
-    # return best_word
+    
