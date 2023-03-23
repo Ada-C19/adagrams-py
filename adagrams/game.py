9d3381a61295457e05908968a25ada243733ba62
Selene Castillo <secast@live.com>
2023-03-23 15:46:54 -0700

    made final changes, ready to submit

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,6 @@
 import random
 from collections import Counter
 
-word_list = ["AAAAAAAAAA", "BBBBBB", "CCC", "DDDD"]
-
 LETTER_POOL = {
     'A': 9, 
     'B': 2, 
@@ -76,58 +74,64 @@ def draw_letters():
         letter_counts = Counter(letter_list)
         if letter not in letter_list:
             letter_list.append(letter)
+# only add letter after chekcing its limit
         elif letter_counts[letter] < LETTER_POOL[letter]:
             letter_list.append(letter)
-        # else:
-        #     continue
     return letter_list
     
 def uses_available_letters(word, letter_bank):
+# create new dicts with letters and their counts
     letter_dict = Counter(letter_bank)
-    cap_word_dict = Counter(list(word.upper()))
-    for char, char_count in cap_word_dict.items():
-        if cap_word_dict[char] <= letter_dict[char]:
-            print(letter_dict[char])
+    word_dict = Counter(list(word.upper()))
+# loop for every letter/character thats in the word
+    for char in word_dict.keys():
+# check to see if the letter from the word is present enough times
+        if word_dict[char] <= letter_dict[char]:
+            letter_dict[char]
         else:
             return False
     return True
     
 
 def score_word(word):
+# create a space for all the scores and ................................
     scores = []
     total = 0
+# loop for ever letter/character in the word
     for char in list(word.upper()):
+# retrieve the letter value and add to scores list
         scores.append(LETTER_SCORES[char])
+# add extra points to total if word is 7 - 10 characters long
     if len(scores) >= 7 and len(scores) <= 10:
         total += 8
-        for value in scores:
-            total += value
-    else:
-        for value in scores:
-            total += value
+# sum all values and add to total
+    for value in scores:
+        total += value
     return total
 
 def get_highest_word_score(word_list):
-# we will collect all words from word_list and use it as a key and its score will be the value
+# collect all words from word_list and use it as a key and its score will be the value
     all_scores_dict = {}
     for word in word_list:
         all_scores_dict[word] = score_word(word)
-# we are defining a space for the word with the highest score
+# variable for the word with the highest score
     highest_word = [None, 0]
-
-# we are looping through the dictionary that has all words and their scores
+# loop through the dictionary that has all words and their scores
     for word, score in all_scores_dict.items():
-# tuple is reassigned if score is higher
+# assign the tuple values of words with the highest scores
         if score > highest_word[1]:
-            print(f"{score} is higher than {highest_word[1]}")
             highest_word[0] = word
             highest_word[1] = score
+# if there is a tie for the highest score
         elif score == highest_word[1]:
+# if the currently highest score word is 10 chars long, keep it
             if len(highest_word[0]) == 10:
                 continue
+# if the incomming word is 10 char long, update tuple
             if len(word) == 10:
                 highest_word[0] = word
                 highest_word[1] = score
+# if none are 10 chars long, keep the shortest word in tuple
             elif len(word) < len(highest_word[0]):
                 highest_word[0] = word
                 highest_word[1] = score
@@ -135,5 +139,3 @@ def get_highest_word_score(word_list):
             continue
 
     return tuple(highest_word)
-                
-print(get_highest_word_score(word_list))
