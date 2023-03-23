e4b4c3be1b9316105bc9ee985a4c53d6222b6963
Madison Jackson <madi.jackson29@gmail.com>
2023-03-22 17:58:27 -0700

    "SORRY! Had to fix something, but NOW I finished!"

--- adagrams/game.py
+++ adagrams/game.py
@@ -29,9 +29,6 @@ LETTER_POOL = {
     'Z': 1
 }
 
-
-
-
 def draw_letters():  
     full_hand = []
     my_hand = []
@@ -76,7 +73,86 @@ def uses_available_letters(word, letter_bank):
     return False
 
 def score_word(word):
-    pass
+    #make a score chart via dictionary that holds the point values of the letters
+    #make a statement that if the word's length is > 7 it get 8+ points
+    #for each letter in the word, take the value and add it to the score_board
+    LETTER_SCORE = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+    }
+    score_board = 0
+    bonus = 8 
+    word = word.upper()
+    for letter in word:
+        if letter in LETTER_SCORE:
+            score_board += LETTER_SCORE[letter]
+    if len(word) >= 7:
+        score_board += bonus
+            
+    return score_board
 
 def get_highest_word_score(word_list):
-    pass
+    #iterate each word in word list, obtaining the scores of each word 
+    #compare the scores; take the best word and score to store it in a tuple
+    #make a list holding the scores themselves
+    #remove indices until the greater one remains 
+    #compare the dict[keys] to the remainder in list
+    #if the value and index match, create a tuple that returns the best word with score
+    #find the max
+    #loop through list to see who has max
+    #make a list to put in possible winners
+    #compare list to len of word (shortest wins) tiebreaker
+    word_dict = {}
+    winners_list = []
+    len_list = []
+    for word in word_list:
+        score = score_word(word)
+        word_dict[word] = score
+
+    max_value = max(word_dict.values())
+
+    for key in word_dict:
+        if word_dict[key] == max_value:
+            test_tuple = (key, max_value)
+            winners_list.append(test_tuple)
+            continue
+
+    for str, value in winners_list:
+        length = len(str)
+        len_list.append(length)
+
+    min_tie = min(len_list)
+
+    
+    if len(winners_list) > 0:
+        for winner in winners_list:
+            if len(winner[0]) >= 10:
+                return winner 
+            else: 
+                continue
+        if len(winner[0]) == min_tie:
+                return winner
