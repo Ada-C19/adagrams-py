920177e0bff1739c31d26eba61d14124bc8dde54
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-03-22 22:48:02 -0700

    i changed the acore chart to a dict and added a score variable.

--- adagrams/game.py
+++ adagrams/game.py
@@ -72,14 +72,23 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    score_chart = {1 : ["A","E", "I", "O", "U", "L", "N", "R", "S", "T" ],
-                   2 :["D","G"],
-                   3 : ["B", "C", "M", "P" ],
-                   4 : ["F", "H", "V", "W", "Y"],
-                   5 : ["K"],
-                   8 : ["J", "X"],
-                   10 : ["Q", "Z"]
-                   }
+    #find out what is best to store letter/point. 
+    score_chart = {
+        "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, 
+        "N": 1, "R": 1, "S": 1, "T": 1, "D": 2,"G": 2,
+        "B": 3, "C": 3, "M": 3, "P": 3,
+        "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
+        "K": 5,
+        "J": 8, "X": 8,
+        "Q": 10, "Z": 10}
+    print(type(score_chart))
+
+    score = 0
+    # 
+    for letter in word:
+        for key,value in score_chart.items():
+        
+
     pass
 
 def get_highest_word_score(word_list):
