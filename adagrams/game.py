27e61614fda3a7724745ad57ad33516e6802709e
Amber Shay <mashaad21@gmail.com>
2023-03-23 23:19:55 -0600

    Returns tuple of word and highest score

--- adagrams/game.py
+++ adagrams/game.py
@@ -31,6 +31,17 @@ LETTER_POOL = {
     'Z': 1
 }
 
+letter_value = {
+    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+    2: ['D', 'G'],
+    3: ['B', 'C', 'M', 'P'],
+    4: ['F', 'H', 'V', 'W', 'Y'],
+    5: ['K'],
+    8: ['J', 'X'],
+    10: ['Q', 'Z']
+}
+
+
 def draw_letters():
     """Build a hand of 10 letters for the user"""
     letter_pool_copy = LETTER_POOL.copy()
@@ -38,15 +49,16 @@ def draw_letters():
 
     while len(letters) < 10:
         letter_key = random.choice(list(letter_pool_copy.keys()))
-
         if letter_pool_copy[letter_key] != 0:
             letter_pool_copy[letter_key] -= 1
             letters.append(letter_key)
 
     return letters
 
+
 def uses_available_letters(word, letter_bank):
     """Take word input and compare to letter bank."""
+
     letter_bank_copy = letter_bank.copy()
     letter_bank_case = [element.upper() for element in letter_bank_copy]
 
@@ -59,17 +71,9 @@ def uses_available_letters(word, letter_bank):
 
     return True
 
+
 def score_word(word):
     """Assign a point value to the input word"""
-    letter_value = {
-        1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-        2: ['D', 'G'],
-        3: ['B', 'C', 'M', 'P'],
-        4: ['F', 'H', 'V', 'W', 'Y'],
-        5: ['K'],
-        8: ['J', 'X'],
-        10: ['Q', 'Z']
-    }
 
     current_score = 0
     for letter in word:
@@ -82,43 +86,21 @@ def score_word(word):
 
     return current_score
 
-words_list = ['graze', 'ferment', 'add', 'sex', 'tenletters', 'branxzorfl']
-def get_word_scores(word_list):
-    """Create a list of words and their scores based on the hand drawn."""
-    word_score_dict = {}
-
-    for word in word_list:
-        score = score_word(word)
-        word_score_dict.update({word: score})
-    score_list = list(word_score_dict.items())
-
-    return score_list
 
 def get_highest_word_score(word_list):
     """Return the highest scoring word as a tuple: ('string', word_score)"""
-    # word_score_dict = {}
 
-    # for word in word_list:
-    #     score = score_word(word)
-    #     word_score_dict.update({word: score})
-    # score_list = list(word_score_dict.items())
-    word_scores = get_word_scores(word_list)
+    highest_score = 0
+    winning_word = ""
 
-    high_score = 0
-    highest_score_words = []
+    for word in word_list:
+        # if the score of the word is higher than highest_score:
+        if score_word(word) > highest_score:
+            # that word's score becomes the high schore
+            highest_score = score_word(word)
+            winning_word = word                    # that word becomes the winning word
 
-    for tupl in word_scores:
-        idx = tupl[1]
-        if idx > high_score:
-            high_score = idx
-            highest_score_words.append(tupl)
-
-    # print(word_scores)
-    # print(high_score)
-    # print(highest_score_words)
-
-    # return tuple of the word with the highest score
-    return highest_score_words[-1]
+    return (winning_word, highest_score)
 
 
-print(get_highest_word_score(words_list))
+print(get_highest_word_score(word_list))
