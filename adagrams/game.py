ea34da655f5cd89240c946299420a1ef02c62e73
Kira Sidhu <kira.kaur.sidhu@gmail.com>
2023-03-24 10:27:43 -0700

    Reformat

--- adagrams/game.py
+++ adagrams/game.py
@@ -124,7 +124,7 @@ def get_highest_word_score(word_list):
         elif score == highest_score:
             if len(highest_word) == 10:
                 continue
-            elif len(word) == 10 or len(word) < len(highest_word):
+            elif len(word.strip()) == 10 or len(word.strip()) < len(highest_word):
                 highest_word, highest_score = word, score
 
     return highest_word, highest_score
