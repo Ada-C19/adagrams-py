830c6108afa6371ffd1742fcf6029f61e142cc76
Zhuo <zhuo.parse@gmail.com>
2023-03-23 21:05:11 -0400

    Completed adagrams

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,35 +1,39 @@
 import random
-LETTER_POOL = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
-}
+
 def draw_letters():
+
+    LETTER_POOL = {
+        'A': 9, 
+        'B': 2, 
+        'C': 2, 
+        'D': 4, 
+        'E': 12, 
+        'F': 2, 
+        'G': 3, 
+        'H': 2, 
+        'I': 9, 
+        'J': 1, 
+        'K': 1, 
+        'L': 4, 
+        'M': 2, 
+        'N': 6, 
+        'O': 8, 
+        'P': 2, 
+        'Q': 1, 
+        'R': 6, 
+        'S': 4, 
+        'T': 6, 
+        'U': 4, 
+        'V': 2, 
+        'W': 2, 
+        'X': 1, 
+        'Y': 2, 
+        'Z': 1
+    }
+
     draw_letters_list = []
     letter_freq = {}
+    
     while len(draw_letters_list) < 10:
         cur_random_letter = random.choice(list(LETTER_POOL.keys()))
         if cur_random_letter in letter_freq.keys():
@@ -41,6 +45,7 @@ def draw_letters():
         else:
             draw_letters_list. append(cur_random_letter)
     return draw_letters_list
+
 def uses_available_letters(word, letter_bank):
     letter_bank_freq = {}
     for letter in letter_bank:
