601d472692aed8100ac109fb1f377e5e38536372
Moyo Moz <moyotornai@gmail.com>
2023-03-23 21:48:47 -0700

    test had indent error, line 76 or 78 word variable was slightly out of order

--- tests/test_wave_04.py
+++ tests/test_wave_04.py
@@ -73,10 +73,10 @@ def test_get_highest_word_tie_prefers_ten_letters_unsorted_list():
     assert best_word[0] == "AAAAAAAAAA"
     assert best_word[1] == 18
 
-    # Arrange
-    words = ["AAAAAAAAAA", "EEEEEEEEEE"]
+   
 def test_get_highest_word_tie_same_length_prefers_first():
-
+ # Arrange
+    words = ["AAAAAAAAAA", "EEEEEEEEEE"]
     # Act
     best_word = get_highest_word_score(words)
 
