7d546b49ba99b149f0232fb44e48de1d1404798b
Sabria <sabriaford23@gmail.com>
2023-03-23 17:34:13 -0700

    wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -71,7 +71,49 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    upper_case_word = word.upper()
+    total = 0
+    score_chart = {"A": 1,
+        "B": 3,
+        "C": 3,
+        "D": 2,
+        "E": 1,
+        "F": 4,
+        "G": 2,
+        "H": 4,
+        "I": 1,
+        "J": 8,
+        "K": 5,
+        "L": 1,
+        "M": 3,
+        "N": 1,
+        "O": 1,
+        "P": 3,
+        "Q": 10,
+        "R": 1,
+        "S": 1,
+        "T": 1,
+        "U": 1,
+        "V": 4,
+        "W": 4,
+        "X": 8,
+        "Y": 4,
+        "Z": 10
+
+    }
+    for letter in upper_case_word:
+        total += score_chart[letter]
+
+    if len(word) >= 7:
+        total += 8
+    
+    return total
+        #add up each letter and return totl
+    #Returns an integer representing the number of points    
+    #The number of points of each letter is summed up to represent the total score of word
+    
+    #If the length of the word is 7, 8, 9, or 10, then the word gets an additional 8 points
+    #if letter in word length is <= 7 +8
 
 def get_highest_word_score(word_list):
     pass
