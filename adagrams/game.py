973561c3581e832a17346544c9c373e4193c21ac
Ariel Higuera <purpl3m00ns@gmail.com>
2023-03-31 10:00:23 -0700

    modifications implementing Matt's feedback

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,4 +1,4 @@
-from random import choices
+from random import sample
 
 LETTER_POOL = {
     "A": 9,
@@ -28,8 +28,8 @@ LETTER_POOL = {
     "Y": 2,
     "Z": 1,
 }
-ONLY_LETTERS = [letter for letter in LETTER_POOL.keys()]
-ONLY_WEIGHTS = [num for num in LETTER_POOL.values()]
+ONLY_LETTERS = list(LETTER_POOL.keys())
+ONLY_WEIGHTS = list(LETTER_POOL.values())
 LETTER_VALUE = {
     1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
     2: ["D", "G"],
@@ -40,17 +40,26 @@ LETTER_VALUE = {
     10: ["Q", "Z"],
 }
 
+# helper function:
+def build_letter_to_score(input_dict):
+    '''return a dictionary with each letter matched with it's score.'''
+    letter_to_score = {}
+    for score, letters in input_dict.items():
+        for letter in letters:
+            letter_to_score[letter] = score
+    return letter_to_score
+  
 
 def draw_letters():
-    ten_letters = ""
-    while len(ten_letters) < 10:
-        letter = choices(ONLY_LETTERS, ONLY_WEIGHTS, k=1)[0]
-        if ten_letters.count(letter) < LETTER_POOL[letter]:
-            ten_letters += letter
+    '''return a list of ten letters from an alphabet pool.'''
+    # uses random.sample which takes the list ONLY_LETTERS,
+    # and returns a list of 10 (k=10) elements in that list, weighted by ONLY_WEIGHTS.
+    ten_letters = sample(ONLY_LETTERS, counts = ONLY_WEIGHTS, k=10)
     return ten_letters
 
 
 def uses_available_letters(word, letter_bank):
+    '''verify the input word uses only letters in the player's hand.'''
     letter_bank_count = {}
     for letter in letter_bank:
         letter_bank_count[letter] = letter_bank.count(letter)
@@ -63,21 +72,19 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
+    '''return the total score of a word by adding the value of each letter'''
+    letter_to_score = build_letter_to_score(LETTER_VALUE)
     total_score = 0
-    for element in word.upper():
-        for value, letters in LETTER_VALUE.items():
-            if element in letters:
-                total_score += value
+    for letter in word.upper():
+        total_score += letter_to_score[letter]
     if len(word) > 6:
         total_score += 8
     return total_score
-
+# n for time complexity: n is chars in word - letter_values is constant, not a variable, that loop doesn't count
 
 def get_highest_word_score(word_list):
-    words_and_scores = {}
-    for word in word_list:
-        words_and_scores[word] = score_word(word)
-
+    '''Given a list of words, return the highest scoring word'''
+    words_and_scores = { word: score_word(word) for word in word_list }
     highest_score = max(words_and_scores.values())
     words_with_high_score = [
         word for word, score in words_and_scores.items() if score == highest_score
@@ -85,5 +92,6 @@ def get_highest_word_score(word_list):
     for word in words_with_high_score:
         if len(word) == 10:
             return word, highest_score
-    return min((word for word in words_with_high_score if word), key=len), highest_score
+    return min((words_with_high_score), key=len), highest_score
 
+# 3n * m => n * m for time complexity.
