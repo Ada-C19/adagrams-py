85baaf554c32d592a47b1e64c9f3ff295c6574d1
alyssa <tn.alyssa@gmail.com>
2023-03-31 17:30:53 -0500

    refactored make_scores_and_word_dictionary along with find_top_score_and_best_words to reduce time complexity by using scores as keys

--- adagrams/game.py
+++ adagrams/game.py
@@ -73,17 +73,15 @@ def draw_letters():
     letter_pool_list = generate_letter_pool()
     letter_bank = []
     for letter in range(0, 10):
-        random_letter_index = random.randint(0, len(letter_pool_list) - 1)
-        letter_bank.append(letter_pool_list[random_letter_index])
-        letter_pool_list.pop(random_letter_index)
+        random_index = random.randint(0, len(letter_pool_list) - 1)
+        letter_bank.append(letter_pool_list[random_index])
+        letter_pool_list.pop(random_index)
     return letter_bank
 
 
 def count_available_letters(letter_bank):
     """Return a dictionary containing the quantity of each letter in letter_bank."""
-    letter_counts = {} 
-    for letter in letter_bank:
-        letter_counts[letter] = 0
+    letter_counts = {letter: 0 for letter in letter_bank}
     for letter in letter_bank:
         letter_counts[letter] += 1
     return letter_counts
@@ -110,27 +108,25 @@ def score_word(word):
     return score
 
 
-def make_word_and_score_dictionary(word_list):
+def make_scores_and_words_dictionary(word_list):
     """Score all words and return dictionary with words and scores."""
-    words_and_scores = {}
+    scores_and_words = {}
     for word in word_list:
-        words_and_scores[word] = score_word(word)
-    return words_and_scores
+        score = score_word(word)
+        if score not in scores_and_words:
+            scores_and_words[score] = [word]
+            continue
+        scores_and_words[score].append(word)
+    return scores_and_words
 
 
 def find_top_score_and_best_words(word_list):
     """Find the top scoring words and return them along with corresponding score."""
-    words_and_scores = make_word_and_score_dictionary(word_list)
-    words_with_highest_scores = []
+    scores_and_words = make_scores_and_words_dictionary(word_list)
+    highest_score = max(scores_and_words.keys())
+    return scores_and_words[highest_score], highest_score
 
-    highest_score = max(words_and_scores.values())
-
-    for word, score in words_and_scores.items():
-        if score == highest_score:
-            words_with_highest_scores.append(word)
-    return words_with_highest_scores, highest_score
     
-
 def get_highest_word_score(word_list):
     """Find the best word out of the words with the top score. Return as tuple with its score."""
     words_to_compare, highest_score = find_top_score_and_best_words(word_list)
