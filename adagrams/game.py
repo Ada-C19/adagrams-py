85312f3225f284bebef5b923092498c93f99626b
Bella <bellaiam@uw.edu>
2023-04-05 09:28:10 -0700

    modified based on feedback

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,6 @@
 import random
 from collections import Counter
+# letter_pool is a dict saving letters and their frequencies 
 LETTER_POOL = {
     'A': 9, 
     'B': 2, 
@@ -28,80 +29,120 @@ LETTER_POOL = {
     'Y': 2, 
     'Z': 1
 }
+SCORES = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4,
+    'G': 2, 
+    'H': 4,
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3,
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8,
+    'Y': 4, 
+    'Z': 10
+}
+
+# def draw_letters():
+#     # Create an empty list to hold the drawn letters
+#     hand = []
+    
+#     # Create a dictionary to keep track of the count of each letter drawn
+#     letter_counts = {letter: 0 for letter in LETTER_POOL}
+    
+#     # Draw 10 letters, one at a time
+#     for i in range(10):
+#         # Randomly select a letter from the list of keys in the LETTER_POOL dictionary
+#         letter = random.choice(list(LETTER_POOL.keys()))
+        
+#         # If the letter count is less than its value in LETTER_POOL, add it to the hand
+#         if letter_counts[letter] < LETTER_POOL[letter]:
+#             hand.append(letter)
+#             letter_counts[letter] += 1
+#         # Otherwise, try again
+#         else:
+#             i -= 1
+    
+#     # Return the list of drawn letters
+#     return hand
 
 def draw_letters():
-    hand = random.sample(list(LETTER_POOL.keys()), 10)
+    # Create an empty list to hold the drawn letters
+    hand = []
+    
+    # Create a list of all letters in the LETTER_POOL dictionary
+    all_letters = []
+    for letter, freq in LETTER_POOL.items():
+        all_letters.extend([letter] * freq)
+    
+    # Shuffle the list of letters and draw 10 at random
+    random.shuffle(all_letters)
+    hand = all_letters[:10]
+    
+    # Return the list of drawn letters
     return hand
 
-# def uses_available_letters(word, letter_bank):
-#     # Convert word to uppercase
-#     word = word.upper()
-#     # Create a counter of the letter_bank
-#     bank_counts = Counter(letter_bank)
-#     # Iterate over each letter in the word
-#     for letter in word:
-#         # If the letter is not in the bank_counts, or if the count is 0, return False
-#         if letter not in letter_bank or bank_counts[letter] == 0:
-#             return False
-#         # Otherwise, decrement the count of the letter in bank_counts
-#         bank_counts[letter] -= 1
-#     # If we make it through the loop without returning False, return True
-#     return True
+
 def uses_available_letters(word, letter_bank):
-    word = word.upper()
-    word_counts = Counter(word)
+    # Convert letter_bank to string
+    letter_bank_str = ''.join(letter_bank)
+
+    # Create a counter for the letters in the word, which counts the number of occurrences of each letter
+    word_counts = Counter(word.upper())
+
+    # Create a counter for the letters in the letter bank, which counts the number of occurrences of each letter
+    bank_counts = Counter(letter_bank_str.upper())
+
+    # Loop over the letters and their counts in the word counter
     for letter, count in word_counts.items():
-        if count > letter_bank.count(letter):
+        # Check if the count of the letter in the word is greater than the count of the letter in the letter bank
+        if count > bank_counts[letter]:
+            # If so, the word cannot be made from the letter bank, so return False
             return False
+    
+    # If all letters in the word can be made from the letter bank, return True
     return True
 
 def score_word(word):
-    # word = word.upper()
-    # score = 0
-    # for letter in word:
-    #     if letter in "AEIOULNRST":
-    #         score += 1
-    #     elif letter in "DG":
-    #         score += 2
-    #     elif letter in "BCMP":
-    #         score += 3
-    #     elif letter in "FHVWY":
-    #         score += 4
-    #     elif letter == "K":
-    #         score += 5
-    #     elif letter in "JX":
-    #         score += 8
-    #     elif letter in "QZ":
-    #         score += 10
-    # if len(word) >= 7:
-    #     score += 8
-    # return score
-    scores = {
-        'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4,
-        'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3,
-        'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8,
-        'Y': 4, 'Z': 10}
-    score = sum(scores.get(letter, 0) for letter in word.upper())
+    """
+    Returns the score for a given word. The word should be a string of letters (can be lowercase or uppercase).
+    """
+    
+    # Calculate the score for the word
+    score = 0
+    for letter in word.upper():
+        try:
+            score += SCORES[letter]
+        except KeyError:
+            score += 0
+    
+    # Add 8 points for words of length 7 or more
     if len(word) >= 7:
         score += 8
+    
     return score
 
 
-
-# def get_highest_word_score(word_list):
-#     word_dict = {}
-#     for word in word_list:
-#         word_dict[word] = score_word(word)
-#     max_val = max(word_dict.values())
-#     max_word = max(word_dict, key=word_dict.get)
-#     for word, score in word_dict.items():
-#         if score == max_val:
-#             if len(max_word) == 10:
-#                 return max_word, max_val
-#             elif len(word) == 10 or len(word) < len(max_word):
-#                 max_word = word
-#     return max_word, max_val
 def get_highest_word_score(word_list):
+    if not word_list:
+        # Handle the case where word_list is empty
+        raise ValueError("The word list is empty")
+    
     # Calculate the score of each word in the list
     word_scores = [(word, score_word(word)) for word in word_list]
 
